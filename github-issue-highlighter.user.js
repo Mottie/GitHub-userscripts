4a73eb989a0f28ed67438418289a92e9313d28c6
Rob Garrison <wowmotty@gmail.com>
2016-05-21 19:27:38 -0500

    Issue-Highlighter: Add GitHub issue highlighter userscript

---
+++ github-issue-highlighter.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name          GitHub Issue Highlighter
+// @version       1.0.0
+// @description   A userscript that highlights the linked-to comment
+// @license       https://creativecommons.org/licenses/by-sa/4.0/
+// @namespace     http://github.com/Mottie
+// @include       https://github.com/*
+// @run-at        document-idle
+// @grant         GM_addStyle
+// @author        Rob Garrison
+// @updateURL     https://raw.githubusercontent.com/Mottie/Github-userscripts/master/github-highlight-comment.user.js
+// @downloadURL   https://raw.githubusercontent.com/Mottie/Github-userscripts/master/github-highlight-comment.user.js
+// ==/UserScript==
+/* global GM_addStyle */
+/*jshint unused:true */
+(function() {
+  "use strict";
+
+  GM_addStyle([
+    // !important needed to override styles added by
+    // https://github.com/StylishThemes/GitHub-Dark
+    ".timeline-comment.selected { border-color: #4183C4 !important; }",
+    ".timeline-comment.selected:before { border-right-color: #4183C4 !important; }"
+  ].join(""));
+
+  var init = function(event) {
+    if (document.querySelector("#discussion_bucket")) {
+      var target, indx,
+        hash = window.location.hash;
+      // remove "selected" class on hashchange
+      if (event) {
+        target = document.querySelectorAll(".timeline-comment");
+        indx = target.length;
+        while (indx--) {
+          target[indx].classList.remove("selected");
+        }
+      }
+      // add "selected" class
+      if (/^#issue(comment)?-\d+/.test(hash)) {
+        target = document.querySelector(hash);
+        if (target) {
+          target.classList.add("selected");
+        }
+      }
+    }
+  };
+
+  window.addEventListener("hashchange", init);
+  init();
+
+})();
