b5bef8391295faa72a62487c0caf0e40e593bb65
Rob Garrison <wowmotty@gmail.com>
2016-09-17 19:53:52 -0500

    Toggle-Expanders: initial commit

---
+++ github-toggle-expanders.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name          GitHub Toggle Expanders
+// @version       1.0.0
+// @description   A userscript that toggles all expanders when one expander is shift-clicked
+// @license       https://creativecommons.org/licenses/by-sa/4.0/
+// @namespace     https://github.com/Mottie
+// @include       https://github.com/*
+// @run-at        document-idle
+// @author        Rob Garrison
+// @updateURL     https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-toggle-expander.user.js
+// @downloadURL   https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-toggle-expander.user.js
+// ==/UserScript==
+/* jshint esnext:true, unused:true */
+(() => {
+	"use strict";
+
+	function toggle(el) {
+		const selector = ".commits-list-item, .js-details-container",
+			state = closest(el, selector).classList.contains("open");
+		Array.from(document.querySelectorAll(selector)).forEach(el => {
+			el.classList[state ? "add" : "remove"]("open");
+		});
+	}
+
+	function closest(el, selector) {
+		while (el && el.nodeName !== "BODY" && !el.matches(selector)) {
+			el = el.parentNode;
+		}
+		return el && el.matches(selector) ? el : [];
+	}
+
+	document.body.addEventListener("click", event => {
+		const target = event.target;
+		if (
+			target && event.getModifierState("Shift") &&
+			target.matches(".ellipsis-expander")
+		) {
+			// give GitHub time to add the class
+			setTimeout(() => {
+				toggle(target);
+			}, 100);
+		}
+	});
+
+})();
