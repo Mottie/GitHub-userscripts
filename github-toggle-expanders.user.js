fc7ccd7184c25bf5c8c82de836e1179cd52ee2ae
Rob Garrison <wowmotty@gmail.com>
2016-09-29 13:20:12 -0500

    Toggle-Expanders: Target selectors inside of commit lists. Fixes #8

--- github-toggle-expanders.user.js
+++ github-toggle-expanders.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          GitHub Toggle Expanders
-// @version       1.0.1
+// @version       1.0.2
 // @description   A userscript that toggles all expanders when one expander is shift-clicked
 // @license       https://creativecommons.org/licenses/by-sa/4.0/
 // @namespace     https://github.com/Mottie
@@ -15,8 +15,11 @@
 	"use strict";
 
 	function toggle(el) {
-		const selector = ".commits-list-item, .js-details-container",
-			state = closest(el, selector).classList.contains("open");
+		const state = closest(el, ".commits-list-item, .js-details-container")
+			.classList.contains("open"),
+			// target buttons inside commits_bucket - fixes #8
+			selector = `.commits-listing .commits-list-item,
+				#commits_bucket .js-details-container`;
 		Array.from(document.querySelectorAll(selector)).forEach(el => {
 			el.classList[state ? "add" : "remove"]("open");
 		});
