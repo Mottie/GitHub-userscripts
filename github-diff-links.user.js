0f32e275b389f6da22780b64ce49cd75d969dbb9
Rob Garrison <wowmotty@gmail.com>
2017-03-24 23:06:28 -0500

    Diff-links: Optimize observers & fix linting

--- github-diff-links.user.js
+++ github-diff-links.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        GitHub Diff Links
-// @version     1.1.2
+// @version     1.2.0
 // @description A userscript that adds links to diff and pull request headers to jump back & forth between files
 // @license     https://creativecommons.org/licenses/by-sa/4.0/
-// @namespace   http://github.com/Mottie
+// @namespace   https://github.com/Mottie
 // @include     https://github.com/*
 // @run-at      document-idle
 // @grant       GM_addStyle
@@ -11,20 +11,22 @@
 // @updateURL   https://raw.githubusercontent.com/Mottie/Github-userscripts/master/github-diff-links.user.js
 // @downloadURL https://raw.githubusercontent.com/Mottie/Github-userscripts/master/github-diff-links.user.js
 // ==/UserScript==
-/* jshint esnext:true, unused:true */
 (() => {
 	"use strict";
 
 	// sometimes tooltips are too narrow...
 	GM_addStyle(".gh-diff-links:after { min-width: 120px; }");
 
+	let debounce,
+		busy = false,
+		observers = [];
+
 	const button = document.createElement("a"),
 		// button [ InnerHTML, tooltip ]
 		nextBtn = ["Next", "Jump to next file\n("],
 		prevBtn = ["Prev", "Jump to previous file\n(" ];
 
-	button.className = "btn btn-sm tooltipped tooltipped-s tooltipped-multiline" +
-		"gh-diff-links";
+	button.className = "btn btn-sm tooltipped tooltipped-s tooltipped-multiline gh-diff-links";
 	button.setAttribute("rel", "nofollow");
 
 	function addButton(el, content, link) {
@@ -52,21 +54,13 @@
 	}
 
 	function addLinks() {
+		busy = true;
 		let last,
-			diffLinks = $$(".gh-diff-links"),
-			links = $$(`
-				#toc ol.content li > a,
-				.pr-toolbar .toc-select .select-menu-item
-			`);
-		// add diff links if they don't already exist
-		if (links.length !== diffLinks.length) {
-			// remove old links (just in case)
-			diffLinks.forEach(el => {
-				el.parentNode.removeChild(el);
-			});
-			// links & file-actions "should" be the same length
-			last = links.length - 1;
-			$$(".file-actions").forEach((el, indx) => {
+			links = $$("#toc ol.content li > a, .pr-toolbar .toc-select .select-menu-item");
+		// links & file-actions "should" be the same length
+		last = links.length - 1;
+		$$(".file-actions").forEach((el, indx) => {
+			if (!$(".gh-diff-links", el)) {
 				if (indx === 0) {
 					addButton(el, nextBtn, links[indx + 1]);
 					addSpace(el, prevBtn);
@@ -78,12 +72,51 @@
 					addButton(el, nextBtn, links[indx + 1]);
 					addButton(el, prevBtn, links[indx - 1]);
 				}
+			}
+		});
+		busy = false;
+	}
+
+	function removeObservers() {
+		observers.forEach(observer => {
+			if (observer) {
+				observer.disconnect();
+			}
+		});
+		observers = [];
+	}
+
+	function addObservers() {
+		// Observe progressively loaded content
+		$$(".js-diff-progressive-container, .js-diff-load-container").forEach(target => {
+			const obsrvr = new MutationObserver(mutations => {
+				mutations.forEach(mutation => {
+					// preform checks before adding code wrap to minimize function calls
+					const tar = mutation.target;
+					if (!busy && tar && (
+							tar.classList.contains("js-diff-load-container") ||
+							tar.classList.contains("blob-wrapper")
+						)
+					) {
+						clearTimeout(debounce);
+						debounce = setTimeout(() => {
+							addLinks();
+						}, 500);
+					}
+				});
 			});
-		}
+			obsrvr.observe(target, {
+				childList: true,
+				subtree: true
+			});
+			observers.push(obsrvr);
+		});
 	}
 
 	function init() {
+		removeObservers();
 		if ($("#files.diff-view") || $(".pr-toolbar")) {
+			addObservers();
 			addLinks();
 		}
 	}
@@ -91,11 +124,12 @@
 	function $(selector, el) {
 		return (el || document).querySelector(selector);
 	}
+
 	function $$(selector, el) {
 		return Array.from((el || document).querySelectorAll(selector));
 	}
 
+	// DOM targets - to detect GitHub dynamic ajax page loading
 	document.addEventListener("pjax:end", init);
 	init();
-
 })();
