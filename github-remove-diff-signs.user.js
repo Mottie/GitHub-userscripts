0aa6d7e390a7506038e09e085a6e2491b104316a
Rob Garrison <wowmotty@gmail.com>
2017-03-24 22:15:38 -0500

    Remove-Diff: Optimize observers & fix #15

--- github-remove-diff-signs.user.js
+++ github-remove-diff-signs.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name          GitHub Remove Diff Signs
-// @version       1.0.2
+// @version       1.1.0
 // @description   A userscript that removes the "+" and "-" from code diffs
 // @license       https://creativecommons.org/licenses/by-sa/4.0/
-// @namespace     http://github.com/Mottie
+// @namespace     https://github.com/Mottie
 // @include       https://github.com/*
 // @grant         none
 // @run-at        document-idle
@@ -11,30 +11,33 @@
 // @updateURL     https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-remove-diff-signs.user.js
 // @downloadURL   https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-remove-diff-signs.user.js
 // ==/UserScript==
-/* jshint unused:true, esnext:true */
-(function() {
+(() => {
 	"use strict";
 
-	let targets,
-		busy = false;
+	let debounce,
+		busy = false,
+		observers = [];
 
 	function processDiff() {
 		busy = true;
 		if (document.querySelector(".highlight")) {
 			let indx = 0,
 
-				els = document.querySelectorAll(".blob-code-deletion .blob-code-inner, .blob-code-addition .blob-code-inner"),
+				els = document.querySelectorAll(`
+					.blob-code-deletion .blob-code-inner,
+					.blob-code-addition .blob-code-inner`
+				),
 				len = els.length,
 
 				// target "+" and "-" at start
 				regex = /^[+-]/,
 
 				// loop with delay to allow user interaction
-				loop = function() {
+				loop = () => {
 					let el, txt,
 						// max number of DOM insertions per loop
 						max = 0;
-					while ( max < 20 && indx < len ) {
+					while ( max < 50 && indx < len ) {
 						if (indx >= len) {
 							return;
 						}
@@ -45,7 +48,7 @@
 						indx++;
 					}
 					if (indx < len) {
-						setTimeout(function(){
+						setTimeout(() => {
 							loop();
 						}, 200);
 					}
@@ -55,32 +58,58 @@
 		busy = false;
 	}
 
+	// GitHub pjax
+	document.addEventListener("pjax:end", init);
+
+	function removeObservers() {
+		observers.forEach(observer => {
+			if (observer) {
+				observer.disconnect();
+			}
+		});
+		observers = [];
+	}
+
+	function addObservers() {
+		// DOM targets - to detect GitHub dynamic ajax page loading
+		Array.from(
+			// Observe progressively loaded content
+			document.querySelectorAll(`
+				.js-diff-progressive-container,
+				.js-diff-load-container`
+			)
+		).forEach(target => {
+			const obsrvr = new MutationObserver(mutations => {
+				mutations.forEach(mutation => {
+					// preform checks before adding code wrap to minimize function calls
+					const tar = mutation.target;
+					if (!busy && tar && (
+							tar.classList.contains("js-diff-load-container") ||
+							tar.classList.contains("blob-wrapper")
+						)
+					) {
+						clearTimeout(debounce);
+						debounce = setTimeout(() => {
+							processDiff();
+						}, 500);
+					}
+				});
+			});
+			obsrvr.observe(target, {
+				childList: true,
+				subtree: true
+			});
+			observers.push(obsrvr);
+		});
+	}
+
 	function init() {
+		removeObservers();
 		if (document.querySelector("#files.diff-view")) {
+			addObservers();
 			processDiff();
 		}
 	}
-
-	// DOM targets - to detect GitHub dynamic ajax page loading
-	targets = document.querySelectorAll([
-		"#js-repo-pjax-container",
-		"#js-pjax-container"
-	].join(","));
-
-	// update TOC when content changes
-	Array.prototype.forEach.call(targets, function(target) {
-		new MutationObserver(function(mutations) {
-			mutations.forEach(function(mutation) {
-				// preform checks before adding code wrap to minimize function calls
-				if (!busy && mutation.target === target) {
-					init();
-				}
-			});
-		}).observe(target, {
-			childList: true,
-			subtree: true
-		});
-	});
 
 	init();
 
