47ce301f4dfe352a63b7d9d937e8fd5145a51507
Rob Garrison <wowmotty@gmail.com>
2017-04-11 06:44:46 -0500

    Code-guides: Fix position accuracy
    Alignment is still slightly off. See #10

--- github-code-guides.user.js
+++ github-code-guides.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub Code Guides
-// @version     1.1.2
+// @version     1.1.3
 // @description A userscript that allows you to add one or more vertical guidelines to the code
 // @license     https://creativecommons.org/licenses/by-sa/4.0/
 // @author      Rob Garrison
@@ -29,23 +29,46 @@
 		}]),
 		font = GM_getValue("ghcg-font", "Menlo");
 
+	function adjust(val) {
+		return `calc(13px + ${val.toFixed(1)}ch)`;
+	}
+
+	function addDefinition(start, end, color) {
+		return `
+			transparent ${start},
+			${color} ${start},
+			${color} ${end},
+			transparent ${end},
+		`;
+	}
+
 	function addGuides(vals) {
-		let css = "";
+		let css = "",
+			diff = "";
 		// to align the guides *after* the setting, we need to add 1, then add
 		// another 0.1 to give the guide a tiny bit of white space to the left
 		vals.forEach(guide => {
-			let start = parseFloat(guide.chars) + 1.1;
-			// eslint-disable-next-line one-var
-			const size = parseFloat(guide.width) || 0.2,
-				// each line needs to be at least 0.2ch in width to be visible
-				end = (start + (size > 0.2 ? size : 0.2)).toFixed(1),
-				color = guide.color || "rgba(0, 0, 0, .3)";
-			start = start.toFixed(1);
-			css += `transparent ${start}ch, ${color} ${start}ch, ${color} ${end}ch, transparent ${end}ch, `;
+			let start = parseFloat(guide.chars),
+				size = parseFloat(guide.width) || 0.2;
+			const color = guide.color || "rgba(0, 0, 0, .3)";
+			// each line needs to be at least 0.2ch in width to be visible
+			size = size > 0.2 ? size : 0.2;
+			css += addDefinition(adjust(start), adjust(start + size), color);
+
+			// shift start to the left 1ch for diff block alignment
+			start += 1;
+			diff += addDefinition(adjust(start), adjust(start + size), color);
 		});
 		style.textContent = `
+			td.blob-code.blob-code-context .blob-code-inner,
+			td.blob-code.blob-code-addition .blob-code-inner,
+			td.blob-code.blob-code-deletion .blob-code-inner {
+				padding-left: 0;
+				background: linear-gradient(to right, transparent 0%, ${diff} transparent 100%) !important;
+			}
 			span.blob-code-inner {
 				display: block !important;
+				padding-left: 13px;
 			}
 			span.blob-code-inner, td.blob-code-inner:not(.blob-code-hunk) {
 				font-family: "${font}", Consolas, "Liberation Mono", Menlo, Courier, monospace !important;
