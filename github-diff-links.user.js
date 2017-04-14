c68245e6f53ebd49d80eccde0f7196106e26deaa
Rob Garrison <wowmotty@gmail.com>
2017-04-13 20:57:43 -0500

    Diff-links: Use mutations.js & fix links after progressive load

--- github-diff-links.user.js
+++ github-diff-links.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub Diff Links
-// @version     1.2.0
+// @version     1.2.1
 // @description A userscript that adds links to diff and pull request headers to jump back & forth between files
 // @license     https://creativecommons.org/licenses/by-sa/4.0/
 // @author      Rob Garrison
@@ -8,6 +8,7 @@
 // @include     https://github.com/*
 // @run-at      document-idle
 // @grant       GM_addStyle
+// @require     https://greasyfork.org/scripts/28721-mutations/code/mutations.js?version=188043
 // @icon        https://github.com/fluidicon.png
 // @updateURL   https://raw.githubusercontent.com/Mottie/Github-userscripts/master/github-diff-links.user.js
 // @downloadURL https://raw.githubusercontent.com/Mottie/Github-userscripts/master/github-diff-links.user.js
@@ -17,10 +18,6 @@
 
 	// sometimes tooltips are too narrow...
 	GM_addStyle(".gh-diff-links:after { min-width: 120px; }");
-
-	let debounce,
-		busy = false,
-		observers = [];
 
 	const button = document.createElement("a"),
 		// button [ InnerHTML, tooltip ]
@@ -55,69 +52,40 @@
 	}
 
 	function addLinks() {
-		busy = true;
-		let last,
-			links = $$("#toc ol.content li > a, .pr-toolbar .toc-select .select-menu-item");
-		// links & file-actions "should" be the same length
-		last = links.length - 1;
-		$$(".file-actions").forEach((el, indx) => {
-			if (!$(".gh-diff-links", el)) {
-				if (indx === 0) {
-					addButton(el, nextBtn, links[indx + 1]);
-					addSpace(el, prevBtn);
-				} else if (indx === last) {
-					// add dummy "next" button to keep spacing
-					addSpace(el, nextBtn);
-					addButton(el, prevBtn, links[indx - 1]);
-				} else {
-					addButton(el, nextBtn, links[indx + 1]);
-					addButton(el, prevBtn, links[indx - 1]);
+		let last, temp,
+			links = $$(".file-header .file-info a");
+		if (links.length) {
+			// links & file-actions "should" be the same length
+			last = links.length - 1;
+			$$(".file-actions").forEach((el, indx) => {
+				// remove disabled buttons added before progressive
+				// content has completed loading
+				temp = $(".gh-diff-links.disabled", el);
+				if (temp) {
+					temp.parentNode.removeChild(temp);
+					// remove both buttons to allow updating
+					temp = $(".gh-diff-links", el);
+					temp.parentNode.removeChild(temp);
 				}
-			}
-		});
-		busy = false;
-	}
-
-	function removeObservers() {
-		observers.forEach(observer => {
-			if (observer) {
-				observer.disconnect();
-			}
-		});
-		observers = [];
-	}
-
-	function addObservers() {
-		// Observe progressively loaded content
-		$$(".js-diff-progressive-container, .js-diff-load-container").forEach(target => {
-			const obsrvr = new MutationObserver(mutations => {
-				mutations.forEach(mutation => {
-					// preform checks before adding code wrap to minimize function calls
-					const tar = mutation.target;
-					if (!busy && tar && (
-							tar.classList.contains("js-diff-load-container") ||
-							tar.classList.contains("blob-wrapper")
-						)
-					) {
-						clearTimeout(debounce);
-						debounce = setTimeout(() => {
-							addLinks();
-						}, 500);
+				if (!$(".gh-diff-links", el)) {
+					if (indx === 0) {
+						addButton(el, nextBtn, links[indx + 1]);
+						addSpace(el, prevBtn);
+					} else if (indx === last) {
+						// add dummy "next" button to keep spacing
+						addSpace(el, nextBtn);
+						addButton(el, prevBtn, links[indx - 1]);
+					} else {
+						addButton(el, nextBtn, links[indx + 1]);
+						addButton(el, prevBtn, links[indx - 1]);
 					}
-				});
+				}
 			});
-			obsrvr.observe(target, {
-				childList: true,
-				subtree: true
-			});
-			observers.push(obsrvr);
-		});
+		}
 	}
 
 	function init() {
-		removeObservers();
 		if ($("#files.diff-view") || $(".pr-toolbar")) {
-			addObservers();
 			addLinks();
 		}
 	}
@@ -131,6 +99,8 @@
 	}
 
 	// DOM targets - to detect GitHub dynamic ajax page loading
-	document.addEventListener("pjax:end", init);
+	document.addEventListener("ghmo:container", init);
+	document.addEventListener("ghmo:diff", addLinks);
 	init();
+
 })();
