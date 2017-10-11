d181746bbfe34c8af7dc873b98485e21a84ecb3f
Rob Garrison <wowmotty@gmail.com>
2017-10-11 15:39:00 -0500

    Code-show-whitespace: Fix expanded & diff rows

--- github-code-show-whitespace.user.js
+++ github-code-show-whitespace.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub Code Show Whitespace
-// @version     1.1.1
+// @version     1.1.2
 // @description A userscript that shows whitespace (space, tabs and carriage returns) in code blocks
 // @license     MIT
 // @author      Rob Garrison
@@ -78,7 +78,8 @@
 			left: .6em;
 		}
 		/* hide extra leading space added to diffs - see #27 */
-		.diff-table .blob-code-inner .pl-space:first-child {
+		.diff-table td.blob-code-inner .pl-space:first-child,
+		.diff-table .blob-code-context .pl-space:first-child {
 			opacity: 0;
 		}
 	`);
