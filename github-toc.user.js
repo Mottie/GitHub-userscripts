502030401594eca81a7328725b977638c660f78f
Rob Garrison <wowmotty@gmail.com>
2018-04-16 12:51:49 -0500

    TOC: Bump version & update Authors

--- github-toc.user.js
+++ github-toc.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub TOC
-// @version     1.2.13
+// @version     1.2.14
 // @description A userscript that adds a table of contents to readme & wiki pages
 // @license     MIT
 // @author      Rob Garrison
