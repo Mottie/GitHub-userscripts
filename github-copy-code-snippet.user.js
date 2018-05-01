d33f76f04cd63baa8e84cb90a84209efb1feb66c
Rob Garrison <wowmotty@gmail.com>
2018-05-01 11:57:42 -0500

    Copy-code-snippet: Use new clipboard-copy element

--- github-copy-code-snippet.user.js
+++ github-copy-code-snippet.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub Copy Code Snippet
-// @version     0.2.3
+// @version     0.3.0
 // @description A userscript adds a copy to clipboard button on hover of markdown code snippets
 // @license     MIT
 // @author      Rob Garrison
@@ -17,21 +17,19 @@
 (() => {
 	"use strict";
 
+	let copyId = 0;
 	const markdownSelector = ".markdown-body, .markdown-format",
 		codeSelector = "pre:not(.gh-csc-pre)",
 
-		copyButton = document.createElement("button");
-
-	addAttr(copyButton, {
-		"aria-label": "Copy to clipboard",
-		className: "js-zeroclipboard btn btn-sm zeroclipboard-button tooltipped tooltipped-w gh-csc-button",
-		"data-copied-hint": "Copied!",
-		type: "button",
-		innerHTML: `
-			<svg aria-hidden="true" class="octicon octicon-clippy" height="16" viewBox="0 0 14 16" width="14">
-				<path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path>
-			</svg>`
-	});
+		copyButton = document.createElement("clipboard-copy");
+	copyButton.className = "btn btn-sm btn-blue tooltipped tooltipped-w gh-csc-button";
+	copyButton.setAttribute("aria-label", "Copy to clipboard");
+	// This hint isn't working yet (GitHub needs to fix it)
+	copyButton.setAttribute("data-copied-hint", "Copied!");
+	copyButton.innerHTML = `
+		<svg aria-hidden="true" class="octicon octicon-clippy" height="16" viewBox="0 0 14 16" width="14">
+			<path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path>
+		</svg>`;
 
 	GM_addStyle(`
 		.gh-csc-wrap {
@@ -42,6 +40,7 @@
 		}
 		.gh-csc-button {
 			display: none;
+			padding: 3px 6px;
 			position: absolute;
 			top: 3px;
 			right: 3px;
@@ -55,26 +54,15 @@
 		}
 	`);
 
-	function addAttr(el, attrs) {
-		Object.keys(attrs).forEach(attr => {
-			const value = attrs[attr];
-			switch(attr) {
-			case "className":
-				el.className = value;
-				break;
-			case "innerHTML":
-				el.innerHTML = value;
-				break;
-			default:
-				el.setAttribute(attr, value);
-			}
-		});
-	}
-
 	function addButton(wrap, code) {
-		wrap.classList.add("gh-csc-wrap", "js-zeroclipboard-container");
-		code.classList.add("gh-csc-code", "js-zeroclipboard-target");
-		wrap.insertBefore(copyButton.cloneNode(true), wrap.childNodes[0]);
+		if (!wrap.classList.contains("gh-csc-wrap")) {
+			copyId++;
+			// See comments from sindresorhus/refined-github/issues/1278
+			code.id = `gh-csc-${copyId}`;
+			copyButton.setAttribute("for", `gh-csc-${copyId}`);
+			wrap.classList.add("gh-csc-wrap");
+			wrap.insertBefore(copyButton.cloneNode(true), wrap.childNodes[0]);
+		}
 	}
 
 	function init() {
