a9bd290852bc90609ebc71d9a613195ffa0147b6
Rob Garrison <wowmotty@gmail.com>
2018-07-16 08:25:10 -0500

    Code-colors: Speed up DOM interaction
    Code cleanup & start using `requestAnimationFrame` with a generator

--- github-code-colors.user.js
+++ github-code-colors.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub Code Colors
-// @version     1.1.16
+// @version     1.2.0
 // @description A userscript that adds a color swatch next to the code color definition
 // @license     MIT
 // @author      Rob Garrison
@@ -20,50 +20,65 @@
 
 	GM.addStyle(`
 		.ghcc-block { width:12px; height:12px; display:inline-block;
-			vertical-align:middle; margin-right:4px; border:1px solid #555; }
+			vertical-align:middle; margin-right:4px; border-radius:3px;
+			border:1px solid rgba(119, 119, 119, 0.5); }
 	`);
 
 	const namedColors = [
-			"aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige",
-			"bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown",
-			"burlywood", "cadetblue", "chartreuse", "chocolate", "coral",
-			"cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan",
-			"darkgoldenrod", "darkgray", "darkgrey", "darkgreen", "darkkhaki",
-			"darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred",
-			"darksalmon", "darkseagreen", "darkslateblue", "darkslategray",
-			"darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue",
-			"dimgray", "dimgrey", "dodgerblue", "firebrick", "floralwhite",
-			"forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod",
-			"gray", "grey", "green", "greenyellow", "honeydew", "hotpink",
-			"indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush",
-			"lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan",
-			"lightgoldenrodyellow", "lightgray", "lightgrey", "lightgreen",
-			"lightpink", "lightsalmon", "lightseagreen", "lightskyblue",
-			"lightslategray", "lightslategrey", "lightsteelblue", "lightyellow",
-			"lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine",
-			"mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen",
-			"mediumslateblue", "mediumspringgreen", "mediumturquoise",
-			"mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin",
-			"navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange",
-			"orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise",
-			"palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum",
-			"powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue",
-			"saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna",
-			"silver", "skyblue", "slateblue", "slategray", "slategrey", "snow",
-			"springgreen", "steelblue", "tan", "teal", "thistle", "tomato",
-			"turquoise", "violet", "wheat", "white", "whitesmoke", "yellow",
-			"yellowgreen"
-		].join("|"),
+		"aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige",
+		"bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown",
+		"burlywood", "cadetblue", "chartreuse", "chocolate", "coral",
+		"cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan",
+		"darkgoldenrod", "darkgray", "darkgrey", "darkgreen", "darkkhaki",
+		"darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred",
+		"darksalmon", "darkseagreen", "darkslateblue", "darkslategray",
+		"darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue",
+		"dimgray", "dimgrey", "dodgerblue", "firebrick", "floralwhite",
+		"forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod",
+		"gray", "grey", "green", "greenyellow", "honeydew", "hotpink",
+		"indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush",
+		"lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan",
+		"lightgoldenrodyellow", "lightgray", "lightgrey", "lightgreen",
+		"lightpink", "lightsalmon", "lightseagreen", "lightskyblue",
+		"lightslategray", "lightslategrey", "lightsteelblue", "lightyellow",
+		"lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine",
+		"mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen",
+		"mediumslateblue", "mediumspringgreen", "mediumturquoise",
+		"mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin",
+		"navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange",
+		"orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise",
+		"palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum",
+		"powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue",
+		"saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna",
+		"silver", "skyblue", "slateblue", "slategray", "slategrey", "snow",
+		"springgreen", "steelblue", "tan", "teal", "thistle", "tomato",
+		"turquoise", "violet", "wheat", "white", "whitesmoke", "yellow",
+		"yellowgreen"
+	].join("|");
 
-		// don't use a div, because GitHub-Dark adds a :hover background
-		// color definition on divs
-		block = document.createElement("span");
+	const regexNamed = new RegExp("^(" + namedColors + ")$", "i");
+	// Ex: #123, #123456 or 0x123456 (unix style colors, used by three.js)
+	const regexHex = /^(#|0x)([0-9A-F]{6,8}|[0-9A-F]{3,4})$/i;
+	// Ex: rgb(0,0,0) or rgba(0,0,0,0.2)
+	const regexRGB = /^rgba?(\([^\)]+\))?/i;
+	const regexRGBA = /rgba/i;
+	// Ex: hsl(0,0%,0%) or hsla(0,0%,0%,0.2);
+	const regexHSL = /^hsla?(\([^\)]+\))?/i;
+
+	// Misc regex
+	const regexQuotes = /['"]/g;
+	const regexUnix = /^0x/;
+	const regexPercent = /%%/g;
+
+	// Don't use a div, because GitHub-Dark adds a :hover background
+	// color definition on divs
+	const block = document.createElement("span");
 	block.className = "ghcc-block";
 
 	function addNode(el, val) {
 		const node = block.cloneNode();
 		node.style.backgroundColor = val;
-		// don't add node if color is invalid
+		// Don't add node if color is invalid
 		if (node.style.backgroundColor !== "") {
 			el.insertBefore(node, el.childNodes[0]);
 		}
@@ -73,104 +88,87 @@
 		return el ? el.textContent : "";
 	}
 
+	function rgb(els, el, txt) {
+		// Color in a string contains everything
+		if (el.classList.contains("pl-s")) {
+			txt = txt.match(regexRGB)[0];
+		} else {
+			// Rgb(a) colors contained in multiple "pl-c1" spans
+			let indx = regexRGBA.test(txt) ? 4 : 3;
+			const tmp = [];
+			while (indx) {
+				tmp.push(getTextContent(els.shift()));
+				indx--;
+			}
+			txt += "(" + tmp.join(",") + ")";
+		}
+		addNode(el, txt);
+		return els;
+	}
+
+	function hsl(els, el, txt) {
+		const tmp = /a$/i.test(txt);
+		if (el.classList.contains("pl-s")) {
+			// Color in a string contains everything
+			txt = txt.match(regexHSL)[0];
+		} else {
+			// Traverse this HTML... & els only contains the pl-c1 nodes
+			// <span class="pl-c1">hsl</span>(<span class="pl-c1">1</span>,
+			// <span class="pl-c1">1</span><span class="pl-k">%</span>,
+			// <span class="pl-c1">1</span><span class="pl-k">%</span>);
+			// using getTextContent in case of invalid css
+			txt = txt + "(" + getTextContent(els.shift()) + "," +
+				getTextContent(els.shift()) + "%," +
+				// Hsla needs one more parameter
+				getTextContent(els.shift()) + "%" +
+				(tmp ? "," + getTextContent(els.shift()) : "") + ")";
+		}
+		// Sometimes (previews only?) the .pl-k span is nested inside
+		// the .pl-c1 span, so we end up with "%%"
+		addNode(el, txt.replace(regexPercent, "%"));
+		return els;
+	}
+
+	// Loop with delay to allow user interaction
+	function* addBlock(els) {
+		while (els.length) {
+			let el = els.shift();
+			let txt = el.textContent;
+			if (el.parentNode.classList.contains("pl-c1")) {
+				// Ignore nested pl-c1 (see https://git.io/vFx8y)
+			} else if (!el.querySelector(".ghcc-block")) {
+				if (el.classList.contains("pl-s")) {
+					txt = txt.replace(regexQuotes, "");
+				}
+				if (regexHex.test(txt) || regexNamed.test(txt)) {
+					addNode(el, txt.replace(regexUnix, "#"));
+				} else if (regexRGB.test(txt)) {
+					els = rgb(els, el, txt);
+				} else if (regexHSL.test(txt)) {
+					els = hsl(els, el, txt);
+				}
+			}
+			yield els;
+		}
+	}
+
 	function addColors() {
 		if (document.querySelector(".highlight")) {
-			let indx = 0;
-			const regexNamed = new RegExp("^(" + namedColors + ")$", "i"),
-				// #123, #123456 or 0x123456 (unix style colors, used by three.js)
-				regexHex = /^(#|0x)([0-9A-F]{6,8}|[0-9A-F]{3,4})$/i,
-				// rgb(0,0,0) or rgba(0,0,0,0.2)
-				regexRGB = /^rgba?(\([^\)]+\))?/i,
-				regexRGBA = /rgba/i,
-				// hsl(0,0%,0%) or hsla(0,0%,0%,0.2);
-				regexHSL = /^hsla?(\([^\)]+\))?/i,
-
-				// misc regex
-				regexQuotes = /['"]/g,
-				regexUnix = /^0x/,
-				regexPercent = /%%/g,
-
-				// .pl-c1 targets css hex colors, "rgb" and "hsl"
-				els = document.querySelectorAll(".pl-c1, .pl-s"),
-				len = els.length;
-
-			// loop with delay to allow user interaction
+			let status;
+			// .pl-c1 targets css hex colors, "rgb" and "hsl"
+			const els = [...document.querySelectorAll(".pl-c1, .pl-s")];
+			const iter = addBlock(els);
 			const loop = () => {
-				let el, txt, tmp, indx2,
-					// max number of DOM insertions per loop
-					max = 0;
-				while (max < 20 && indx < len) {
-					if (indx >= len) {
-						return;
-					}
-					el = els[indx];
-					txt = el.textContent;
-					if (el.classList.contains("pl-s")) {
-						txt = txt.replace(regexQuotes, "");
-					} else if (el.parentNode.classList.contains("pl-c1")) {
-						// ignore nested pl-c1 (see https://git.io/vFx8y)
-						indx++;
-						continue;
-					}
-					if (regexHex.test(txt) || regexNamed.test(txt)) {
-						if (!el.querySelector(".ghcc-block")) {
-							addNode(el, txt.replace(regexUnix, "#"));
-							max++;
-						}
-					} else if (regexRGB.test(txt)) {
-						if (!el.querySelector(".ghcc-block")) {
-							// color in a string contains everything
-							if (el.classList.contains("pl-s")) {
-								txt = txt.match(regexRGB)[0];
-							} else {
-								// rgb(a) colors contained in multiple "pl-c1" spans
-								indx2 = regexRGBA.test(txt) ? 4 : 3;
-								tmp = [];
-								while (indx2) {
-									tmp.push(getTextContent(els[++indx]));
-									indx2--;
-								}
-								txt += "(" + tmp.join(",") + ")";
-							}
-							addNode(el, txt);
-							max++;
-						}
-					} else if (regexHSL.test(txt)) {
-						if (!el.querySelector(".ghcc-block")) {
-							tmp = /a$/i.test(txt);
-							if (el.classList.contains("pl-s")) {
-								// color in a string contains everything
-								txt = txt.match(regexHSL)[0];
-							} else {
-								// traverse this HTML... & els only contains the pl-c1 nodes
-								// <span class="pl-c1">hsl</span>(<span class="pl-c1">1</span>,
-								// <span class="pl-c1">1</span><span class="pl-k">%</span>,
-								// <span class="pl-c1">1</span><span class="pl-k">%</span>);
-								// using getTextContent in case of invalid css
-								txt = txt + "(" + getTextContent(els[++indx]) + "," +
-								getTextContent(els[++indx]) + "%," +
-								// hsla needs one more parameter
-								getTextContent(els[++indx]) + "%" +
-								(tmp ? "," + getTextContent(els[++indx]) : "") + ")";
-							}
-							// sometimes (previews only?) the .pl-k span is nested inside
-							// the .pl-c1 span, so we end up with "%%"
-							addNode(el, txt.replace(regexPercent, "%"));
-							max++;
-						}
-					}
-					indx++;
+				for (let i = 0; i < 40; i++) {
+					status = iter.next();
 				}
-				if (indx < len) {
-					setTimeout(() => {
-						loop();
-					}, 200);
+				if (!status.done) {
+					requestAnimationFrame(loop);
 				}
 			};
 			loop();
 		}
 	}
-
 	document.addEventListener("ghmo:container", addColors);
 	document.addEventListener("ghmo:preview", addColors);
 	addColors();
