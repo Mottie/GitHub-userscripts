07fa5c68466080d4a87c3b120640c6a18be284c5
Rob Garrison <wowmotty@gmail.com>
2018-08-15 10:13:08 -0500

    Toggle-wiki-sidebar: Add button margin & code cleanup

--- github-toggle-wiki-sidebar.user.js
+++ github-toggle-wiki-sidebar.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub Toggle Wiki Sidebar
-// @version     1.0.13
+// @version     1.0.14
 // @description A userscript that adds a button to toggle the GitHub Wiki sidebar
 // @license     MIT
 // @author      Rob Garrison
@@ -19,16 +19,24 @@
 	"use strict";
 
 	// disable click targeting of button SVG internals
-	GM_addStyle(".ghtws-button > * { pointer-events: none; }");
+	// classes "mr-1" = 4px & "mr-2" = 8px; silly GitHub
+	GM_addStyle(`
+		.ghtws-button > * { pointer-events: none; }
+		.ghtws-button { margin-right: 6px; }`
+	);
 
 	// sidebar state
-	let isHidden = false,
+	let isHidden = false;
 
-		toggleIcon = "<svg class='octicon' xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'><path fill='none' stroke='currentColor' stroke-miterlimit='10' d='M.5 3.5h10v9H.5z'/><path fill='currentColor' stroke='currentColor' stroke-miterlimit='10' d='M7 7.8l1.5-1.2V9zM10.5 3.5h5v9h-5v-9zm4.3 4.3l-4.3-3V11l4.3-3.2z'/></svg>";
+	const toggleIcon = `
+		<svg class="octicon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+			<path fill="none" stroke="currentColor" stroke-miterlimit="10" d="M.5 3.5h10v9H.5z"/>
+			<path fill="currentColor" stroke="currentColor" stroke-miterlimit="10" d="M7 7.8l1.5-1.2V9zM10.5 3.5h5v9h-5v-9zm4.3 4.3l-4.3-3V11l4.3-3.2z"/>
+		</svg>`;
 
 	function addToggle() {
-		if (document.querySelector("#wiki-wrapper") && !document.querySelector(".ghtws-button")) {
-			let el = document.querySelector(".gh-header-actions") || document.querySelector(".gh-header-title");
+		if ($("#wiki-wrapper") && !$(".ghtws-button")) {
+			let el = $(".gh-header-actions") || $(".gh-header-title");
 			const button = document.createElement("div");
 			button.className = "btn btn-sm tooltipped tooltipped-s ghtws-button";
 			button.innerHTML = toggleIcon;
@@ -48,7 +56,7 @@
 	}
 
 	function toggleSidebar() {
-		const sidebar = document.querySelector("#wiki-rightbar"),
+		const sidebar = $("#wiki-rightbar"),
 			wrapper = sidebar && sidebar.parentNode;
 		if (sidebar) {
 			if (isHidden) {
@@ -70,9 +78,13 @@
 		}
 	}
 
+	function $(selector, el) {
+		return (el || document).querySelector(selector);
+	}
+
 	function init() {
 		isHidden = GM_getValue("sidebar-state", false);
-		document.querySelector("body").addEventListener("click", toggleEvent);
+		$("body").addEventListener("click", toggleEvent);
 		addToggle();
 	}
 
