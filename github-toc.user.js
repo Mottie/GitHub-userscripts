f8562ee7f8fd069726980b26aa50bb9eaa30d4a3
OttNorml <ottnorml@gmail.com>
2019-04-23 19:43:22 +0200

    Update TOC position due to Github design update (compare PR #43 && #55)

--- github-toc.user.js
+++ github-toc.user.js
@@ -26,7 +26,7 @@
 
 	GM.addStyle(`
 		/* z-index > 1000 to be above the */
-		.ghus-toc { position:fixed; z-index:1001; min-width:200px; top:60px; right:10px; }
+		.ghus-toc { position:fixed; z-index:1001; min-width:200px; top:64px; right:10px; }
 		.ghus-toc h3 { cursor:move; }
 		/* icon toggles TOC container & subgroups */
 		.ghus-toc h3 svg, .ghus-toc li.collapsible .ghus-toc-icon { cursor:pointer; vertical-align:baseline; }
