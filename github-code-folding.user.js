af7fbf3f23aea032080c67cdb554b4fa8533bc81
Rob Garrison <wowmotty@gmail.com>
2019-06-07 07:31:22 -0500

    Code-folding: Update selectors, support gists & cleanup.
    Fixes #86

--- github-code-folding.user.js
+++ github-code-folding.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name        GitHub Code Folding
-// @version     1.0.18
+// @version     1.1.0
 // @description A userscript that adds code folding to GitHub files
 // @license     MIT
 // @author      Rob Garrison
 // @namespace   https://github.com/Mottie
 // @include     https://github.com/*
+// @include     https://gist.github.com/*
 // @run-at      document-idle
 // @grant       GM.addStyle
 // @grant       GM_addStyle
@@ -25,74 +26,70 @@
 
 	GM.addStyle(`
 		td.blob-code.blob-code-inner { position:relative; padding-left:10px; }
-		.collapser { position:absolute; left:2px; width:22px; opacity:.5;
+		.ghcf-collapser { position:absolute; left:2px; width:22px; opacity:.5;
 			transition:.15s; cursor:pointer; }
-		.collapser:after { content:"\u25bc"; }
-		.collapser:hover { opacity:1; }
-		.sideways { transform:rotate(-90deg); transform-origin:16% 49%; opacity:.8; }
-		.hidden-line { display:none; }
-		.ellipsis { padding:1px 2px; margin-left:2px; cursor:pointer;
+		.ghcf-collapser:after { content:"\u25bc"; }
+		.ghcf-collapser:hover { opacity:1; }
+		.ghcf-collapsed { transform:rotate(-90deg); transform-origin:16% 49%; opacity:.8; }
+		.ghcf-hidden-line { display:none; }
+		.ghcf-ellipsis { padding:1px 2px; margin-left:2px; cursor:pointer;
 			background:rgba(255,235,59,.4); position:relative; z-index:1; }
-		.ellipsis:hover { background:rgba(255,235,59,.7); }
+		.ghcf-ellipsis:hover { background:rgba(255,235,59,.7); }
 	`);
 
-	const pairs = new Map(),
-		ellipsis = document.createElement("span"),
-		triangle = document.createElement("span");
-	let isMobile = false;
+	const blocks = {};
+	const ellipsis = document.createElement("span");
+	const triangle = document.createElement("span");
 
-	triangle.className = "collapser";
-	ellipsis.className = "pl-smi ellipsis";
+	triangle.className = "ghcf-collapser";
+	ellipsis.className = "pl-smi ghcf-ellipsis";
 	ellipsis.innerHTML = "&hellip;";
 
 	function countInitialWhiteSpace(arr) {
 		const getWhiteSpaceIndex = i => {
-			if (
-				arr[i] !== " " && arr[i] !== "\t" &&
-				(!isMobile || arr[i] !== "\xa0")
-			) {
+			if (arr[i] !== " " && arr[i] !== "\t" && arr[i] !== "\xa0") {
 				return i;
 			}
-			i++;
-			return getWhiteSpaceIndex(i);
+			return getWhiteSpaceIndex(++i);
 		};
 		return getWhiteSpaceIndex(0);
 	}
 
 	function getPreviousSpaces(map, lineNum) {
 		let prev = map.get(lineNum - 1);
-		return prev === -1 ?
-			getPreviousSpaces(map, lineNum - 1) : {
+		return prev === -1
+			? getPreviousSpaces(map, lineNum - 1)
+			: {
 				lineNum: lineNum - 1,
 				count: prev
 			};
 	}
 
 	function getLineNumber(el) {
-		let elm = el.closest(isMobile ? "div" : "td"),
-			index = elm ? elm.id : "";
-		if (index) {
-			return parseInt(index.slice(2), 10);
+		let elm = el.closest("tr");
+		if (elm) {
+			elm = elm.querySelector("[data-line-number]");
+			return elm ? parseInt(elm.dataset.lineNumber, 10) : "";
 		}
 		return "";
 	}
 
-	function getCodeLines() {
-		return $$(
-			isMobile ?
-				".blob-file-content .line" :
-				".file table.highlight .blob-code-inner"
-		);
+	function getCodeLines(codeBlock) {
+		return $$(".blob-code-inner", codeBlock);
 	}
 
-	function toggleCode(action, index, depth) {
+	function toggleCode({ action, codeBlock, index, depth }) {
 		let els, lineNums;
-		const codeLines = getCodeLines();
+		const codeLines = getCodeLines(codeBlock) || [];
+		const pairs = blocks[codeBlock.dataset.blockIndex];
+		if (!pairs || codeLines.length === 0) {
+			return;
+		}
 		// depth is a string containing a specific depth number to toggle
 		if (depth) {
-			els = $$(`.collapser[data-depth="${depth}"]`);
+			els = $$(`.ghcf-collapser[data-depth="${depth}"]`, codeBlock);
 			lineNums = els.map(el => {
-				el.classList.toggle("sideways", action === "hide");
+				el.classList.toggle("ghcf-collapsed", action === "hide");
 				return getLineNumber(el);
 			});
 		} else {
@@ -101,16 +98,16 @@
 
 		if (action === "hide") {
 			lineNums.forEach(start => {
-				let elm,
-					end = pairs.get(start - 1);
+				let elm;
+				let end = pairs.get(start - 1);
 				codeLines.slice(start, end).forEach(el => {
-					elm = isMobile ? el : el.closest("tr");
+					elm = el.closest("tr");
 					if (elm) {
-						elm.classList.add("hidden-line");
+						elm.classList.add("ghcf-hidden-line");
 					}
 				});
-				if (!$(".ellipsis", codeLines[start - 1])) {
-					elm = $(".collapser", codeLines[start - 1]);
+				if (!$(".ghcf-ellipsis", codeLines[start - 1])) {
+					elm = $(".ghcf-collapser", codeLines[start - 1]);
 					elm.parentNode.insertBefore(
 						ellipsis.cloneNode(true),
 						elm.nextSibling
@@ -121,17 +118,17 @@
 			lineNums.forEach(start => {
 				let end = pairs.get(start - 1);
 				codeLines.slice(start, end).forEach(el => {
-					let elm = isMobile ? el : el.closest("tr");
+					let elm = el.closest("tr");
 					if (elm) {
-						elm.classList.remove("hidden-line");
-						remove(".ellipsis", elm);
+						elm.classList.remove("ghcf-hidden-line");
+						removeEls(".ghcf-ellipsis", elm);
 					}
-					elm = $(".sideways", elm);
+					elm = $(".ghcf-collapsed", elm);
 					if (elm) {
-						elm.classList.remove("sideways");
+						elm.classList.remove("ghcf-collapsed");
 					}
 				});
-				remove(".ellipsis", codeLines[start - 1]);
+				removeEls(".ghcf-ellipsis", codeLines[start - 1]);
 			});
 		}
 		// shift ends up selecting text on the page, so clear it
@@ -144,84 +141,95 @@
 		document.addEventListener("click", event => {
 			let index, elm, isCollapsed;
 			const el = event.target;
+			const codeBlock = el.closest(".highlight");
 
 			// click on collapser
-			if (el && el.classList.contains("collapser")) {
-				isCollapsed = el.classList.contains("sideways");
+			if (el && el.classList.contains("ghcf-collapser")) {
+				isCollapsed = el.classList.contains("ghcf-collapsed");
 				index = getLineNumber(el);
 				// Shift + click to toggle them all
 				if (index && event.getModifierState("Shift")) {
-					return toggleCode(
-						isCollapsed ? "show" : "hide",
+					return toggleCode({
+						action: isCollapsed ? "show" : "hide",
+						codeBlock,
 						index,
-						el.getAttribute("data-depth")
-					);
+						depth: el.dataset.depth
+					});
 				}
 				if (index) {
 					if (isCollapsed) {
-						el.classList.remove("sideways");
-						toggleCode("show", index);
+						el.classList.remove("ghcf-collapsed");
+						toggleCode({ action: "show", codeBlock, index });
 					} else {
-						el.classList.add("sideways");
-						toggleCode("hide", index);
+						el.classList.add("ghcf-collapsed");
+						toggleCode({ action: "hide", codeBlock, index });
 					}
 				}
 				return;
 			}
 
 			// click on ellipsis
-			if (el && el.classList.contains("ellipsis")) {
-				elm = $(".sideways", el.parentNode);
+			if (el && el.classList.contains("ghcf-ellipsis")) {
+				elm = $(".ghcf-collapsed", el.parentNode);
 				if (elm) {
-					elm.classList.remove("sideways");
+					elm.classList.remove("ghcf-collapsed");
 				}
 				index = getLineNumber(el);
 				if (index) {
-					toggleCode("show", index);
+					toggleCode({ action: "show", codeBlock, index });
 				}
 			}
 		});
 	}
 
 	function addCodeFolding() {
-		if ($(".file table.highlight") || $("div.blob-file-content")) {
-			isMobile = !$(".file table.highlight");
-
-			// In case this script has already been run and modified the DOM on a
-			// previous page in github, make sure to reset it.
-			remove("span.collapser");
-			pairs.clear();
-
-			const codeLines = getCodeLines(),
-				spaceMap = new Map(),
-				stack = [];
-
-			codeLines.forEach((el, lineNum) => {
-				let prevSpaces,
-					line = el.textContent,
-					count = line.trim().length ?
-						countInitialWhiteSpace(line.split("")) :
-						-1;
-				spaceMap.set(lineNum, count);
-
-				function tryPair() {
-					let el,
-						top = stack[stack.length - 1];
-					if (count !== -1 && count <= spaceMap.get(top)) {
-						pairs.set(top, lineNum);
-						// prepend triangle
-						el = triangle.cloneNode();
-						el.setAttribute("data-depth", count + 1);
-						codeLines[top].appendChild(el, codeLines[top].childNodes[0]);
-						stack.pop();
-						return tryPair();
-					}
+		// Keep .file in case someone needs this userscript for GitHub Enterprise
+		if ($(".file table.highlight, .blob-wrapper table.highlight")) {
+			$$("table.highlight").forEach((codeBlock, blockIndex) => {
+				if (codeBlock && codeBlock.classList.contains("ghcf-processed")) {
+					// Already processed
+					return;
 				}
-				tryPair();
+				const codeLines = getCodeLines(codeBlock);
+				removeEls("span.ghcf-collapser", codeBlock);
+				if (codeLines) {
+					// In case this script has already been run and modified the DOM on a
+					// previous page in github, make sure to reset it.
+					codeBlock.classList.add("ghcf-processed");
+					codeBlock.dataset.blockIndex = blockIndex;
 
-				prevSpaces = getPreviousSpaces(spaceMap, lineNum);
-				if (count > prevSpaces.count) {
-					stack.push(prevSpaces.lineNum);
+					const spaceMap = new Map();
+					const stack = [];
+					const pairs = blocks[blockIndex] = new Map();
+
+					codeLines.forEach((el, lineNum) => {
+						let prevSpaces;
+						let line = el.textContent;
+						let count = line.trim().length
+							? countInitialWhiteSpace(line.split(""))
+							: -1;
+						spaceMap.set(lineNum, count);
+
+						function tryPair() {
+							let el;
+							let top = stack[stack.length - 1];
+							if (count !== -1 && count <= spaceMap.get(top)) {
+								pairs.set(top, lineNum);
+								// prepend triangle
+								el = triangle.cloneNode();
+								el.dataset.depth = count + 1;
+								codeLines[top].appendChild(el, codeLines[top].childNodes[0]);
+								stack.pop();
+								return tryPair();
+							}
+						}
+						tryPair();
+
+						prevSpaces = getPreviousSpaces(spaceMap, lineNum);
+						if (count > prevSpaces.count) {
+							stack.push(prevSpaces.lineNum);
+						}
+					});
 				}
 			});
 		}
@@ -235,9 +243,9 @@
 		return Array.from((el || document).querySelectorAll(selector));
 	}
 
-	function remove(selector, el) {
-		let els = $$(selector, el),
-			index = els.length;
+	function removeEls(selector, el) {
+		let els = $$(selector, el);
+		let index = els.length;
 		while (index--) {
 			els[index].parentNode.removeChild(els[index]);
 		}
@@ -245,9 +253,9 @@
 
 	function removeSelection() {
 		// remove text selection - https://stackoverflow.com/a/3171348/145346
-		const sel = window.getSelection ?
-			window.getSelection() :
-			document.selection;
+		const sel = window.getSelection
+			? window.getSelection()
+			: document.selection;
 		if (sel) {
 			if (sel.removeAllRanges) {
 				sel.removeAllRanges();
