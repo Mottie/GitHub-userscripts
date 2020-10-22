6f291c0e9656ff87491b412d0ea0558c55250c5d
Rob Garrison <wowmotty@gmail.com>
2020-10-22 07:50:00 -0500

    Html-preview: prepend link to keep inline

--- github-html-preview.user.js
+++ github-html-preview.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub HTML Preview
-// @version     1.0.1
+// @version     1.0.2
 // @description A userscript that adds preview links to HTML files
 // @license     MIT
 // @author      Rob Garrison
@@ -31,13 +31,14 @@
 		<svg class="octicon v-align-text-bottom" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="currentColor">
 			<path d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/>
 		</svg>`;
+	link.style.marginRight = ".25rem";
 
 	function addLink(el) {
-		const cell = el.closest(".js-navigation-item div[role='rowheader']");
+		const cell = el.closest(".js-navigation-item div[role='rowheader'] span");
 		if (cell && !$(".ghhp-btn", cell)) {
 			const preview = link.cloneNode(true);
 			preview.href = prefix + el.href;
-			cell.appendChild(preview);
+			cell.prepend(preview);
 		}
 	}
 
