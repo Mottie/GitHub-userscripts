38ee04eedcdeeac0b9b517ce37ba5cb9627aa8b4
Sean Zhang <zxf2342@qq.com>
2021-03-30 15:54:51 +0800

    Show days between the latest commit time.

--- github-analyse-forks.user.js
+++ github-analyse-forks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        GitHub Analyse Forks
-// @version     0.1.1
-// @description A userscript that analyzes GitHub forks, helps you to find out the worthiest fork.
+// @version     0.2.0
+// @description A userscript that analyzes GitHub forks, shows compare info between fork repos and parent repo, helps you to find out the worthiest fork.
 // @license     MIT
 // @author      Sean Zhang
 // @namespace   https://github.com/zhangsean
@@ -22,87 +22,96 @@
 
     Date.prototype.format=function(t){var e={"M+":this.getMonth()+1,"d+":this.getDate(),"H+":this.getHours(),"h+":this.getHours(),"m+":this.getMinutes(),"s+":this.getSeconds(),"q+":Math.floor((this.getMonth()+3)/3),S:this.getMilliseconds()};for(var s in/(y+)/.test(t)&&(t=t.replace(RegExp.$1,(this.getFullYear()+"").substr(4-RegExp.$1.length))),e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t};
 
-    const sleep=t=>new Promise(f=>setTimeout(f,t));
+    const sleep=(t)=>new Promise(f=>setTimeout(f,t));
 
-    const gm=(u,f)=>GM_xmlhttpRequest({url:u,onload:f});
+    const gm=(u,f)=>GM_xmlhttpRequest({url:u,onload:(xhr)=>{let h=xhr.responseText;let e=h.indexOf('Whoa there!');if(e>-1)f('Whoa Github limits!');f(h);}});
+
+    const show=(e,b,c=0)=>{e.innerHTML+=" <b"+(0==c?"":' style="color:'+(c>0?"red":"#01cc1b")+';"')+">"+b+"</b>"};
+
+    let myDt = 0; // Latest commit time of current repo
+
+    function analyseDate (el, ix, html) {
+        let i = html.indexOf('datetime');
+        if (i > -1) {
+            let dt = html.substring(i + 10, html.indexOf('"', i + 10));
+            dt = new Date(dt);
+            show(el, dt.format('yyyy-MM-dd HH:mm:ss'));
+            if (ix == 0) {
+                myDt = dt
+            } else {
+                let df = Math.round((dt - myDt) / (1000 * 60 * 60 * 24), 0);
+                show(el, df == 0 ? 'same day' : ((df > 0 ? 'ahead' : 'behind') + ' ' + Math.abs(df) + ' day' + (Math.abs(df) > 1 ? 's' : '')), df);
+            }
+        }
+    }
+
+    function analyse(el, ix) {
+        if(!myDt && ix > 0) {
+            sleep(200).then(()=>analyse(el, ix));
+            return;
+        }
+        // Get fork repo info
+        let repo = $('a:last-child', el).attributes.href.nodeValue;
+        gm(`https://github.com${repo}`, (html)=>{
+            // Meet Github limits
+            let e = html.indexOf('Whoa');
+            if (e > -1) {
+                show(el, html, 1);
+                return;
+            }
+            // Get latest commit time
+            let i = html.indexOf('tree-commit');
+            if (i > -1) {
+                let link = html.substring(html.lastIndexOf('"', i) + 1, html.indexOf('"', i));
+                gm(`https://github.com${link}`, (html)=>{
+                    analyseDate (el, ix, html);
+                });
+            } else {
+                analyseDate (el, ix, html);
+            }
+            // Get fork compare info
+            i = html.indexOf('This branch is');
+            if (i > -1) {
+                let compare = html.substring(i, html.indexOf('.', i));
+                let e = compare.indexOf('even');
+                if (e > -1) {
+                    compare = compare.replace('even', '<b>even</b>');
+                }
+                let a = compare.indexOf('ahead');
+                if (a > -1) {
+                    let num = compare.substring(compare.lastIndexOf('is', a) + 3, compare.lastIndexOf('commit', a) - 1);
+                    compare = compare.replace(num, '<b style="color:red;">' + num + '</b>');
+                }
+                let b = compare.indexOf('behind');
+                if (b > -1) {
+                    let d = compare.lastIndexOf(',', b);
+                    if (d == -1) {
+                        d = compare.lastIndexOf('is', b) + 1;
+                    }
+                    let num = compare.substring(d + 2, compare.lastIndexOf('commit', b) - 1);
+                    compare = compare.replace(num, '<b style="color:#01cc1b;">' + num + '</b>');
+                }
+                el.innerHTML += ' ' + compare;
+            }
+        });
+    }
 
     function init() {
-		const root = $(".network");
-		const repos = $$(".network .repo");
-		if (root && repos.length > 0) {
-            console.log(repos.length);
-            let a = 0;
-            repos.forEach((el, i)=>{
-                let b = ++a;
-                // Get fork repo info
-                let repo = $('a:last-child', el).attributes.href.nodeValue;
-                // Delay 2 minutes every 300 requests to avoid GitHub limits
-                sleep(b + (Math.floor(b / 100) * 120000)).then(()=>{
-                    //console.log('r', b, (new Date()).format('yyyy-MM-dd HH:mm:ss'));
-                    gm(`https://github.com${repo}`, (xhr)=>{
-                            // Get latest commit time
-                            let html = xhr.responseText,
-                                i = html.indexOf('tree-commit');
-                            if (i > -1) {
-                                let link = html.substring(html.lastIndexOf('"', i) + 1, html.indexOf('"', i));
-                                gm(`https://github.com${link}`, (xhr)=>{
-                                    let html = xhr.responseText,
-                                        i = html.indexOf('datetime');
-                                    if (i > -1) {
-                                        let dt = html.substring(i + 10, html.indexOf('"', i + 10));
-                                        dt = new Date(dt).format('yyyy-MM-dd HH:mm:ss');
-                                        el.innerHTML = el.innerHTML + ' <b>' + dt + '</b>';
-                                    }
-                                    let e = html.indexOf('Whoa there!');
-                                    if (e > -1) {
-                                        console.log('res', b, 'Whoa there!');
-                                    }
-                                });
-                            } else {
-                                i = html.indexOf('datetime');
-                                if (i > -1) {
-                                    let dt = html.substring(i + 10, html.indexOf('"', i + 10));
-                                    dt = new Date(dt).format('yyyy-MM-dd HH:mm:ss');
-                                    el.innerHTML = el.innerHTML + ' <b>' + dt + '</b>';
-                                }
-                            }
-                            // Get fork compare info
-                            i = html.indexOf('This branch is');
-                            if (i > -1) {
-                                let compare = html.substring(i, html.indexOf('.', i));
-                                let e = compare.indexOf('even');
-                                if (e > -1) {
-                                    compare = compare.replace('even', '<b>even</b>');
-                                }
-                                let a = compare.indexOf('ahead');
-                                if (a > -1) {
-                                    let num = compare.substring(compare.lastIndexOf('is', a) + 3, compare.lastIndexOf('commit', a) - 1);
-                                    compare = compare.replace(num, '<b style="color:red;">' + num + '</b>');
-                                }
-                                let b = compare.indexOf('behind');
-                                if (b > -1) {
-                                    let d = compare.lastIndexOf(',', b);
-                                    if (d == -1) {
-                                        d = compare.lastIndexOf('is', b) + 1;
-                                    }
-                                    let num = compare.substring(d + 2, compare.lastIndexOf('commit', b) - 1);
-                                    compare = compare.replace(num, '<b style="color:#01cc1b;">' + num + '</b>');
-                                }
-                                el.innerHTML = el.innerHTML + ' ' + compare;
-                            }
-                            let e = html.indexOf('Whoa there!');
-                            if (e > -1) {
-                                console.log('res', b, 'Whoa there!');
-                            }
-                        }
-                    );
+        const root = $(".network");
+        const repos = $$(".network .repo");
+        if (root && repos.length > 0) {
+            console.log('Fork repos', repos.length);
+
+            repos.forEach((el, b)=>{
+                // Delay 1 minute every 100 requests to avoid Github limits
+                sleep(b + (Math.floor(b / 100) * 60000)).then(()=>{
+                    analyse(el, b);
                 });
             });
-		}
-	}
 
-    if (/members/.test(document.location)) {
-        document.addEventListener("pjax:end", init);
-        init();
+        }
     }
+
+    init();
+
 })();
