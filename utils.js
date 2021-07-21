25f7739792881780edefe97c41c8f8f2c8ce6652
Rob Garrison <wowmotty@gmail.com>
2021-07-20 21:32:12 -0500

    Utils: Cleanup

--- utils.js
+++ utils.js
@@ -1,13 +1,13 @@
-/* GitHub userscript utilities v0.1.2-alpha
+/* GitHub userscript utilities v0.2.0
  * Copyright © 2021 Rob Garrison
  * License: MIT
  */
 /* exported
  * $ $$
  * addClass removeClass toggleClass
+ * removeEls removeSelection
  * on off make
  * debounce
- * addMenu
  */
 "use strict";
 
@@ -18,8 +18,66 @@ const REGEX = {
 };
 
 /* DOM utilities */
+/**
+ * Find & return a single DOM node
+ * @param {String} selector - CSS selector string
+ * @param {HTMLElement} el - DOM node to start the query (defaults to document)
+ * @returns {HTMLElement|null}
+ */
 const $ = (selector, el) => (el || document).querySelector(selector);
+
+/**
+ * Find & return multiple DOM nodes
+ * @param {String} selector - CSS selector string
+ * @param {HTMLElement} el - DOM node to start the query (defaults to document) 
+ * @returns {HTMLElement[]}
+ */
 const $$ = (selector, el) => [...(el || document).querySelectorAll(selector)];
+
+/**
+ * Common functions
+ */
+const _ = {};
+/**
+* Return an array of elements
+* @param {HTMLElement|HTMLElement[]|NodeList} elements 
+* @returns {HTMLElement[]}
+*/
+_.createElementArray = elements => {
+	if (Array.isArray(elements)) {
+		return elements;
+	}
+	return elements instanceof NodeList ? [...elements] : [elements];
+};
+/**
+* Common event listener code
+* @param {String} type - "add" or "remove" event listener
+* @param {HTMLElement[]} els - DOM node array that need listeners
+* @param {String} name - Event name, e.g. "click", "mouseover", etc
+* @param {Function} handler - Event callback
+* @param {Object} options - Event listener options or useCapture - see
+*   https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#parameters
+*/
+_.eventListener = (type, els, name, handler, options) => {
+	const events = name.split(REGEX.WHITESPACE);
+	_.createElementArray(els).forEach(el => {
+		events.forEach(ev => {
+			el?.[`${type}EventListener`](ev, handler, options);
+		});
+	});
+};
+/**
+* Create an array of classes/event types from a space or comma separated string
+* @param {String} classes - space or comma separated list of classes or events
+* @returns {String[]}
+*/
+_.getClasses = classes => {
+	if (Array.isArray(classes)) {
+		return classes;
+	}
+	const names = classes.toString();
+	return names.includes(",") ? names.split(REGEX.COMMA) : [names];
+};
 
 /**
  * Add class name(s) to one or more elements
@@ -68,9 +126,40 @@ const toggleClass = (elements, className, flag) => {
 };
 
 /**
+ * Remove DOM nodes
+ * @param {String} selector - CSS selector string
+ * @param {HTMLElement|undefined} el - parent DOM node (defaults to document)
+ */
+const removeEls = (selector, el) => {
+	let els = $$(selector, el);
+	let index = els.length;
+	while (index--) {
+		els[index].parentNode.removeChild(els[index]);
+	}
+};
+
+/**
+ * Remove text selection
+ */
+const removeSelection = () => {
+	// remove text selection - https://stackoverflow.com/a/3171348/145346
+	const sel = window.getSelection
+		? window.getSelection()
+		: document.selection;
+	if (sel) {
+		if (sel.removeAllRanges) {
+			sel.removeAllRanges();
+		} else if (sel.empty) {
+			sel.empty();
+		}
+	}
+};
+
+/**
  * Add/remove event listener
  * @param {HTMLElement|HTMLElement[]|NodeList} els
- * @param {string} name - event name(s) to bind, e.g. "mouseup mousedown"
+ * @param {string} name - event name(s) to bind, e.g. "mouseup mousedown"; also
+ *   accpets a comma separated string, e.g. "mouseup, mousedown"
  * @param {function} handler - event handler
  * @param {options} eventListener options
  */
@@ -81,31 +170,14 @@ const off = (els, name = "", handler, options) => {
 	_.eventListener("remove", els, name, handler, options);
 }
 
-const _ = {};
-_.createElementArray = elements => {
-	if (Array.isArray(elements)) {
-		return elements;
-	}
-	return elements instanceof NodeList ? [...elements] : [elements];
-};
-_.eventListener = (type, els, name, handler, options) => {
-	const events = name.split(REGEX.WHITESPACE);
-	_.createElementArray(els).forEach(el => {
-		events.forEach(ev => {
-			el?.[`${type}EventListener`](ev, handler, options);
-		});
-	});
-};
-_.getClasses = classes => {
-	if (Array.isArray(classes)) {
-		return classes;
-	}
-	const names = classes.toString();
-	return names.includes(",") ? names.split(REGEX.COMMA) : [names];
-};
-
 /**
- * Helpers
+ * **** Helpers ****
+ */
+/**
+ * Debounce
+ * @param {Function} fxn - callback executed after debounce
+ * @param {Number} time - time (in ms) to delay
+ * @returns {Function} debounced function
  */
 const debounce = (fxn, time = 500) => {
 	let timer;
@@ -169,274 +241,3 @@ const make = (obj, children) => {
 	}
 	return el;
 }
-
-/* Add GitHub menu
- * Example set up
-ghMenu.open(
-	"Popup Title",
-	[{
-		name: "Title",
-		type: "text",
-		get: () => GM_getValue("title"),
-		set: value => GM_setValue("title", value)
-	}, {
-		name: "Border width (px)",
-		type: "number",
-		get: () => GM_getValue("border-width"),
-		set: value => GM_setValue("border-width", value)
-	}, {
-		name: "Is enabled?",
-		type: "checkbox",
-		get: () => GM_getValue("enabled"),
-		set: value => GM_setValue("enabled", value)
-	}, {
-		name: "Background Color",
-		type: "color",
-		get: () => GM_getValue("bkg-color"),
-		set: value => GM_setValue("bkg-color", value)
-	}, {
-		name: "Widget enabled",
-		type: "checkbox",
-		get: () => GM_getValue("widget-is-enabled"),
-		set: value => GM_setValue("widget-is-enabled", value)
-	}, {
-		name: "Image choice",
-		type: "select",
-		get: () => GM_getValue("img-choice"),
-		set: value => GM_setValue("img-choice", value),
-		options: [
-			{ label: "Car", value: "/images/car.jpg" },
-			{ label: "Jet", value: "/images/jet.jpg" },
-			{ label: "Cat", value: "/images/cat.jpg" }
-		]
-	}]
-);
-*/
-const ghMenu = {
-	init: () => {
-		if (!$("#ghmenu-style")) {
-			make({
-				el: "style",
-				id: "ghmenu-style",
-				textContent: `
-					#ghmenu, #ghmenu summary { cursor: default; }
-					#ghmenu summary:before { cursor: pointer; }
-					#ghmenu-inner input[type="color"] { border: 0; padding: 0 }
-					#ghmenu-inner ::-webkit-color-swatch-wrapper { border: 0; padding: 0; }
-					#ghmenu-inner ::-moz-color-swatch-wrapper { border: 0; padding: 0; }
-					}
-				`,
-				appendTo: "body"
-			});
-		}
-	},
-
-	open: (title, options) => {
-		if (!$("#ghmenu")) {
-			ghMenu._createMenu(title);
-			ghMenu._options = options;
-		}
-		ghMenu._title = title;
-		ghMenu._addContent(options);
-	},
-	close: event => {
-		if (event) {
-			event.preventDefault();
-		}
-		const menu = $("#ghmenu");
-		if (menu) {
-			menu.remove();
-		}
-	},
-	append: options => {
-		const menu = $("#ghmenu");
-		if (menu) {
-			ghMenu._appendContent(options);
-		} else {
-			ghMenu.open("", options);
-		}
-	},
-	refresh: () => {
-		ghMenu._addContent(ghMenu._options);
-	},
-
-	_types: {
-		_input: (type, eventType, opts) => {
-			const elm = make({
-				el: "input",
-				id: `${opts.id}-input`,
-				className: `ghmenu-${type} ${type === "checkbox"
-					? "m-2"
-					: "form-control input-block width-full"
-				}`,
-				attrs: {
-					type,
-					value: opts.get()
-				},
-			});
-			const handler = e => opts.set(type === "checkbox"
-				? e.target.checked
-				: e.target.value
-			);
-			on(elm, eventType, handler);
-			return elm;
-		},
-		text: opts => ghMenu._types._input("text", "input", opts),
-		number: opts => ghMenu._types._input("number", "input", opts),
-		checkbox: opts => ghMenu._types._input("checkbox", "change", opts),
-		color: opts => ghMenu._types._input("color", "change", opts),
-		radio: opts => {}, // TO DO
-		select: opts => {
-			const elm = make({
-				el: "select",
-				className: "width-full ghmenu-select",
-				attrs: {
-					value: opts.get()
-				}
-			}, opts.options.map(obj => (
-				make({
-					el: "option",
-					text: obj.label,
-					attrs: {
-						value: obj.value
-					}
-				})
-			)));
-			on(elm, "change", e => opts.set(e.target.value));
-			return elm;
-		},
-
-		/* TO DO
-		* - add multiple?
-		*   colors: ['#000', '#fff']
-		*   guideline: { width: '.2', color: '#a00', chars: 80 }
-		* - link to more details/docs?
-		*/
-		group: opts => {
-			const group = opts.group;
-			if (Array.isArray(group) && group.length) {
-				const fragment = document.createDocumentFragment();
-				fragment.appendChild(make({ el: "strong", text: opts.name }));
-				group.forEach(entry => {
-					const row = make({
-						className: "Box-row d-flex flex-row pr-0"
-					}, [
-						ghMenu._createLabel(entry.id, entry.name),
-						make({
-							id,
-							className: `ml-2 no-wrap${
-								// align checkbox to right edge
-								opt.type === "checkbox" ? " d-flex flex-justify-end" : ""
-							}`,
-						})
-					])
-				})
-			}
-		},
-	},
-	_options: [],
-	_createMenu: () => {
-		// create menu
-		make({
-			el: "details",
-			id: "ghmenu",
-			className: "details-reset details-overlay details-overlay-dark lh-default text-gray-dark",
-			attrs: {
-				open: true
-			},
-			html: `
-				<summary role="button" aria-label="Close dialog" />
-				<details-dialog
-					id="ghmenu-dialog"
-					class="Box Box--overlay d-flex flex-column anim-fade-in fast container-xl"
-					role="dialog"
-					aria-modal="true"
-					tab-index="-1"
-				>
-					<div class="readability-extra d-flex flex-auto flex-column overflow-hidden">
-						<div class="Box-header">
-							<h2 id="ghmenu-title" class="Box-title"></h2>
-						</div>
-						<div class="Box-body p-0 overflow-scroll">
-							<div class="container-lg p-responsive advanced-search-form">
-								<fieldset id="ghmenu-inner" class="pb-2 mb-2 min-width-0" />
-							</div>
-						</div>
-					</div>
-					<button id="ghmenu-close-menu" class="Box-btn-octicon m-0 btn-octicon position-absolute right-0 top-0" type="button" aria-label="Close dialog" data-close-dialog="">
-						<svg class="octicon octicon-x" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true">
-							<path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48L7.48 8z" />
-						</svg>
-					</button>
-				</details-dialog>`,
-			appendTo: "body"
-		});
-		on($("#ghmenu-close-menu"), "click", e => ghMenu.close(e), { once: true });
-		on($("#ghmenu summary"), "click", e => {
-			e.preventDefault();
-			e.stopPropagation();
-			const target = e.target;
-			if (target && !target.closest("#ghmenu-dialog")) {
-				ghMenu.close(e);
-			}
-		});
-	},
-	_addContent: options => {
-		const menu = $("#ghmenu-inner");
-		if (menu) {
-			menu.innerHTML = "";
-			ghMenu._appendContent(options);
-		}
-	},
-	/* <dt><label for="{ID}-input">{NAME}</label></dt> */
-	_createLabel: (id, text) => make({
-		el: "dt",
-	}, [
-		make({
-			el: "label",
-			className: "flex-auto",
-			text,
-			attrs: {
-				for: `${id}-input`
-			}
-		})
-	]),
-	_appendContent: options => {
-		const container = $("#ghmenu-inner");
-		if (container) {
-			// update title, if needed
-			$("#ghmenu-title").textContent = ghMenu._title;
-
-			const fragment = document.createDocumentFragment();
-			options.forEach((opt, indx) => {
-				const id = `ghmenu-${opt.name.replace(/\s/g, "")}-${indx}`;
-				const output = opt.type === "group"
-					? ghMenu._types.group({ ...opt, id })
-					: make({
-						el: "dl",
-						className: "form-group flattened d-flex d-md-block flex-column border-bottom my-0 py-2",
-					}, [
-						ghMenu._createLabel(id, opt.name),
-						make({
-							el: "dd",
-							id,
-							className: opt.type === "checkbox"
-								? "d-flex flex-justify-end"
-								: "",
-						}, [
-							ghMenu._types[opt.type || "text"]({ ...opt, id })
-						])
-					]);
-				fragment.appendChild(output);
-			});
-			container.appendChild(fragment);
-		}
-	}
-};
-
-/*
-TESTING
-ghMenu.init();
-ghMenu.open('Testing',[{name:'test color',type:'color',get:()=>'#ddd',set:v=>console.log('setting to',v)},{name:'test number',type:'checkbox',get:()=>true,set:v=>console.log('setting cb to',v)}]);
-ghMenu.append([{name:"Image choice",type:"select",get:()=>"Car",set:v=>console.log("img-choice", v),options:[{label:"Car",value:"/images/car.jpg"},{label:"Jet",value:"/images/jet.jpg"},{label:"Cat",value:"/images/cat.jpg"}]}]);
-*/
