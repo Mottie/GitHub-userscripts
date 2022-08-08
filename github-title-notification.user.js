db1c2daec4c29c0a864bb40c6508f39ab7d08ed4
TFWol <9045213+TFWol@users.noreply.github.com>
2022-08-08 05:33:38 -0700

    FIxed Notification features (#172)
    * Fixed setting custom indicator and interval
    
    * Updated notification visibility logic
    
    From @Mottie
    Updated logic for notification to should show up.

--- github-title-notification.user.js
+++ github-title-notification.user.js
@@ -27,7 +27,7 @@
 	function check() {
 		let title = document.title,
 			mail = document.querySelector(".mail-status"),
-			hasUnread = mail ? mail.classList.contains("unread") : false;
+			hasUnread = mail ? !mail.hidden : false;
 		//
 		if (!/^\(\d+\)/.test(title)) {
 			title = title.replace(/^(\([^)]+\)\s)*/g, "");
@@ -49,14 +49,14 @@
 	GM_registerMenuCommand("Set GitHub Title Notification Indicator", () => {
 		const val = prompt("Indicator Value (it will be wrapped in parentheses)?", indicator);
 		if (val !== null) {
-			GM_setValue("indicator", indicator);
+			GM_setValue("indicator", val);
 			check();
 		}
 	});
 	GM_registerMenuCommand("Set GitHub Title Notification Interval", () => {
 		const val = prompt("Interval Value (in seconds)?", interval);
 		if (val !== null) {
-			GM_setValue("interval", interval);
+			GM_setValue("interval", val);
 			setTimer();
 		}
 	});
