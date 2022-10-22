86513edf539fe88114d3bb27c3c4075de2d37bff
Rob Garrison <wowmotty@gmail.com>
2022-10-22 11:57:15 -0500

    Utils: Update mutations root observer

--- mutations.js
+++ mutations.js
@@ -1,56 +1,60 @@
-/* GitHub mutations observer library script v0.4.4
+/* GitHub mutations observer library script v0.6.0
  * Detect changes to various elements and trigger an event
  * This script is meant to be used as a library for GitHub-based userscripts
- * Copyright © 2021 Rob Garrison
+ * Copyright © 2022 Rob Garrison
  * License: MIT
  */
 (() => {
 	"use strict";
 
 	// prefix for event & document body class name, e.g. "ghmo:container"
-	const prefix = "ghmo",
-		disableAttr = `data-${prefix}-disable`,
-		debounceInterval = 200,
-		targets = {
-			// pjax container (covers general, repo & gists)
-			// .news = newsfeed layout
-			// .repository-content = file code (code folding)
-			"[data-pjax-container], .news, .repository-content": {
-				count: 0,
-				name: "container"
-			},
-			// comment preview active
-			".js-preview-body": {
-				count: 0,
-				name: "preview"
-			},
-			// .js-discussion = wrapper for progressively loaded comments;
-			// "# items not shown" example: https://github.com/isaacs/github/issues/18
-			// .discussion-item = issue status changed (github-issue-show-status)
-			// #progressive-timeline-item-container = load hidden items (old?)
-			// #js-progressive-timeline-item-container = load hidden items
-			".js-discussion, .discussion-item, .toolbar-item, #progressive-timeline-item-container, #js-progressive-timeline-item-container": {
-				count: 0,
-				name: "comments"
-			},
-			// progressively loaded content (diff files)
-			".js-diff-progressive-container, .data.blob-wrapper, .js-diff-load-container, .diff-table tbody": {
-				count: 0,
-				name: "diff"
-			},
-			// issues/pr sidebar & timeline sections: e.g. form actions, commit
-			// references, deployment state & PR checks container
-			".js-updatable-content, .js-updatable-content-preserve-scroll-position": {
-				count: 0,
-				name: "updatable"
-			},
-			// user profile menu (loads on hover)
-			"details-menu": {
-				count: 0,
-				name: "menu"
-			}
+	const prefix = "ghmo";
+	const disableAttr = `data-${prefix}-disable`;
+	const debounceInterval = 250;
+	const targets = {
+		// pjax container (covers general, repo & gists)
+		// .news = newsfeed layout
+		// .repository-content = file code (code folding)
+		// body = global changes
+		"[data-pjax-container], .news, .repository-content, body": {
+			count: 0,
+			name: "container"
 		},
-		list = Object.keys(targets);
+		// comment preview active
+		".js-preview-body": {
+			count: 0,
+			name: "preview"
+		},
+		// .js-discussion = wrapper for progressively loaded comments;
+		// "# items not shown" example: https://github.com/isaacs/github/issues/18
+		// .discussion-item = issue status changed (github-issue-show-status)
+		// #progressive-timeline-item-container = load hidden items (old?)
+		// #js-progressive-timeline-item-container = load hidden items
+		// markdown-toolbar = issue comments
+		".js-discussion, .discussion-item, .toolbar-item, #progressive-timeline-item-container, #js-progressive-timeline-item-container, markdown-toolbar": {
+			count: 0,
+			name: "comments"
+		},
+		// progressively loaded content (diff files)
+		".js-diff-progressive-container, .data.blob-wrapper, .js-diff-load-container, .diff-table tbody, .diff-progressive-loader": {
+			count: 0,
+			name: "diff"
+		},
+		// issues/pr sidebar & timeline sections: e.g. form actions, commit
+		// references, deployment state & PR checks container
+		".js-updatable-content, .js-updatable-content-preserve-scroll-position": {
+			count: 0,
+			name: "updatable"
+		},
+		// user profile menu (loads on hover)
+		"details-menu": {
+			count: 0,
+			name: "menu"
+		}
+	};
+	const list = Object.keys(targets);
+	let queue = new Set();
+	let timer;
 
 	function fireEvents() {
 		list.forEach(selector => {
@@ -58,50 +62,52 @@
 				// event => "ghmo:container", "ghmo:comments"
 				const event = new Event(prefix + ":" + targets[selector].name);
 				document.dispatchEvent(event);
+				targets[selector].count = 0;
 			}
-			targets[selector].count = 0;
 		});
+	}
+
+	function process() {
+		clearTimeout(timer);
+		// avoiding use of forEach loops for performance reasons
+		for (const target of queue) {
+			if (target) {
+				for (const item of list) {
+					if (target.matches(item)) {
+						targets[item].count++;
+					}
+				}
+			}
+		}
+		timer = setTimeout(() => fireEvents(), debounceInterval);
+		queue.clear();
 	}
 
 	function init() {
 		// prevent error when library is loaded at document-start & no body exists
 		const container = document.querySelector("body");
-		let timer;
+		/* document.body attribute used to disable updates; it *should not*
+		 * be used regularly as multiple scripts may enable or disable the
+		 * observers at inappropriate times. It is best that each script handles
+		 * the mutation events triggered by this library on its own
+		 */
+		if (container.getAttribute(disableAttr)) {
+			return;
+		}
 		// prevent script from installing more than once
 		if (container && !container.classList.contains(prefix + "-enabled")) {
 			container.classList.add(prefix + "-enabled");
-			// bound to document.body... this may be bad for performance
 			// http://stackoverflow.com/a/39332340/145346
 			new MutationObserver(mutations => {
-				clearTimeout(timer);
-				/* document.body attribute used to disable updates; it *should not*
-				 * be used regularly as multiple scripts may enable or disable the
-				 * observers at inappropriate times. It is best that each script handles
-				 * the mutation events triggered by this library on its own
-				 */
-				if (container.getAttribute(disableAttr)) {
-					return;
+				if (!queue.size) {
+					requestAnimationFrame(process);
 				}
-				let mindx, target, lindx,
-					llen = list.length,
-					mlen = mutations.length;
-				// avoiding use of forEach loops for performance reasons
-				for (mindx = 0; mindx < mlen; mindx++) {
-					target = mutations[mindx].target;
-					if (target) {
-						for (lindx = 0; lindx < llen; lindx++) {
-							if (target.matches(list[lindx])) {
-								targets[list[lindx]].count++;
-							}
-						}
-					}
-					timer = setTimeout(() => {
-						fireEvents();
-					}, debounceInterval);
+				for (const mutation of mutations) {
+					queue.add(mutation.target);
 				}
-			}).observe(container, {
+			}).observe(document, {
 				childList: true,
-				subtree: true
+				subtree: true,
 			});
 		}
 	}
@@ -111,5 +117,4 @@
 	} else {
 		init();
 	}
-
 })();
