cb7eab6f69bbc4ef6491b4fa5a81dbedb72ea914
Rob Garrison <wowmotty@gmail.com>
2022-10-22 12:43:37 -0500

    Static-time: update dependencies

--- github-static-time.user.js
+++ github-static-time.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        GitHub Static Time
-// @version     1.0.9
+// @version     1.0.10
 // @description A userscript that replaces relative times with a static time formatted as you like it
 // @license     MIT
 // @author      Rob Garrison
@@ -11,142 +11,163 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_registerMenuCommand
-// @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js
-// @require     https://greasyfork.org/scripts/28721-mutations/code/mutations.js?version=952601
+// @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment-with-locales.min.js
+// @require     https://greasyfork.org/scripts/28721-mutations/code/mutations.js?version=1108163
+// @require     https://greasyfork.org/scripts/398877-utils-js/code/utilsjs.js?version=1079637
 // @icon        https://github.githubassets.com/pinned-octocat.svg
 // @updateURL   https://raw.githubusercontent.com/Mottie/Github-userscripts/master/github-static-time.user.js
 // @downloadURL https://raw.githubusercontent.com/Mottie/Github-userscripts/master/github-static-time.user.js
 // @supportURL  https://github.com/Mottie/GitHub-userscripts/issues
 // ==/UserScript==
 (() => {
-	/* global moment */
+	/* global moment $ $$ on */
 	"use strict";
 
-	let busy = false,
-		timeFormat = GM_getValue("ghst-format", "LLL"),
-		locale = GM_getValue("ghst-locale", "en");
+	let busy = false;
+	let timeFormat = GM_getValue("ghst-format", "LLL");
+	let locale = GM_getValue("ghst-locale", "en");
 
 	// list copied from
 	// https://github.com/moment/momentjs.com/blob/master/data/locale.js
 	const locales = [
-			{ "abbr": "af", "name": "Afrikaans" },
-			{ "abbr": "sq", "name": "Albanian" },
-			{ "abbr": "ar", "name": "Arabic" },
-			{ "abbr": "ar-dz", "name": "Arabic (Algeria)" },
-			{ "abbr": "ar-kw", "name": "Arabic (Kuwait)" },
-			{ "abbr": "ar-ly", "name": "Arabic (Lybia)" },
-			{ "abbr": "ar-ma", "name": "Arabic (Morocco)" },
-			{ "abbr": "ar-sa", "name": "Arabic (Saudi Arabia)" },
-			{ "abbr": "ar-tn", "name": "Arabic (Tunisia)" },
-			{ "abbr": "hy-am", "name": "Armenian" },
-			{ "abbr": "az", "name": "Azerbaijani" },
-			{ "abbr": "eu", "name": "Basque" },
-			{ "abbr": "be", "name": "Belarusian" },
-			{ "abbr": "bn", "name": "Bengali" },
-			{ "abbr": "bs", "name": "Bosnian" },
-			{ "abbr": "br", "name": "Breton" },
-			{ "abbr": "bg", "name": "Bulgarian" },
-			{ "abbr": "my", "name": "Burmese" },
-			{ "abbr": "km", "name": "Cambodian" },
-			{ "abbr": "ca", "name": "Catalan" },
-			{ "abbr": "tzm", "name": "Central Atlas Tamazight" },
-			{ "abbr": "tzm-latn", "name": "Central Atlas Tamazight Latin" },
-			{ "abbr": "zh-cn", "name": "Chinese (China)" },
-			{ "abbr": "zh-hk", "name": "Chinese (Hong Kong)" },
-			{ "abbr": "zh-tw", "name": "Chinese (Taiwan)" },
-			{ "abbr": "cv", "name": "Chuvash" },
-			{ "abbr": "hr", "name": "Croatian" },
-			{ "abbr": "cs", "name": "Czech" },
-			{ "abbr": "da", "name": "Danish" },
-			{ "abbr": "nl", "name": "Dutch" },
-			{ "abbr": "nl-be", "name": "Dutch (Belgium)" },
-			{ "abbr": "en-au", "name": "English (Australia)" },
-			{ "abbr": "en-ca", "name": "English (Canada)" },
-			{ "abbr": "en-ie", "name": "English (Ireland)" },
-			{ "abbr": "en-nz", "name": "English (New Zealand)" },
-			{ "abbr": "en-gb", "name": "English (United Kingdom)" },
-			{ "abbr": "en", "name": "English (United States)" },
-			{ "abbr": "eo", "name": "Esperanto" },
-			{ "abbr": "et", "name": "Estonian" },
-			{ "abbr": "fo", "name": "Faroese" },
-			{ "abbr": "fi", "name": "Finnish" },
-			{ "abbr": "fr", "name": "French" },
-			{ "abbr": "fr-ca", "name": "French (Canada)" },
-			{ "abbr": "fr-ch", "name": "French (Switzerland)" },
-			{ "abbr": "fy", "name": "Frisian" },
-			{ "abbr": "gl", "name": "Galician" },
-			{ "abbr": "ka", "name": "Georgian" },
-			{ "abbr": "de", "name": "German" },
-			{ "abbr": "de-at", "name": "German (Austria)" },
-			{ "abbr": "de-ch", "name": "German (Switzerland)" },
-			{ "abbr": "el", "name": "Greek" },
-			{ "abbr": "he", "name": "Hebrew" },
-			{ "abbr": "hi", "name": "Hindi" },
-			{ "abbr": "hu", "name": "Hungarian" },
-			{ "abbr": "is", "name": "Icelandic" },
-			{ "abbr": "id", "name": "Indonesian" },
-			{ "abbr": "it", "name": "Italian" },
-			{ "abbr": "ja", "name": "Japanese" },
-			{ "abbr": "jv", "name": "Javanese" },
-			{ "abbr": "kn", "name": "Kannada" },
-			{ "abbr": "kk", "name": "Kazakh" },
-			{ "abbr": "tlh", "name": "Klingon" },
-			{ "abbr": "gom-latn", "name": "Konkani Latin script" },
-			{ "abbr": "ko", "name": "Korean" },
-			{ "abbr": "ky", "name": "Kyrgyz" },
-			{ "abbr": "lo", "name": "Lao" },
-			{ "abbr": "lv", "name": "Latvian" },
-			{ "abbr": "lt", "name": "Lithuanian" },
-			{ "abbr": "lb", "name": "Luxembourgish" },
-			{ "abbr": "mk", "name": "Macedonian" },
-			{ "abbr": "ms-my", "name": "Malay" },
-			{ "abbr": "ms", "name": "Malay" },
-			{ "abbr": "ml", "name": "Malayalam" },
-			{ "abbr": "dv", "name": "Maldivian" },
-			{ "abbr": "mi", "name": "Maori" },
-			{ "abbr": "mr", "name": "Marathi" },
-			{ "abbr": "me", "name": "Montenegrin" },
-			{ "abbr": "ne", "name": "Nepalese" },
-			{ "abbr": "se", "name": "Northern Sami" },
-			{ "abbr": "nb", "name": "Norwegian Bokmål" },
-			{ "abbr": "nn", "name": "Nynorsk" },
-			{ "abbr": "fa", "name": "Persian" },
-			{ "abbr": "pl", "name": "Polish" },
-			{ "abbr": "pt", "name": "Portuguese" },
-			{ "abbr": "pt-br", "name": "Portuguese (Brazil)" },
-			{ "abbr": "x-pseudo", "name": "Pseudo" },
-			{ "abbr": "pa-in", "name": "Punjabi (India)" },
-			{ "abbr": "ro", "name": "Romanian" },
-			{ "abbr": "ru", "name": "Russian" },
-			{ "abbr": "gd", "name": "Scottish Gaelic" },
-			{ "abbr": "sr", "name": "Serbian" },
-			{ "abbr": "sr-cyrl", "name": "Serbian Cyrillic" },
-			{ "abbr": "sd", "name": "Sindhi" },
-			{ "abbr": "si", "name": "Sinhalese" },
-			{ "abbr": "sk", "name": "Slovak" },
-			{ "abbr": "sl", "name": "Slovenian" },
-			{ "abbr": "es", "name": "Spanish" },
-			{ "abbr": "es-do", "name": "Spanish (Dominican Republic)" },
-			{ "abbr": "sw", "name": "Swahili" },
-			{ "abbr": "sv", "name": "Swedish" },
-			{ "abbr": "tl-ph", "name": "Tagalog (Philippines)" },
-			{ "abbr": "tzl", "name": "Talossan" },
-			{ "abbr": "ta", "name": "Tamil" },
-			{ "abbr": "te", "name": "Telugu" },
-			{ "abbr": "tet", "name": "Tetun Dili (East Timor)" },
-			{ "abbr": "th", "name": "Thai" },
-			{ "abbr": "bo", "name": "Tibetan" },
-			{ "abbr": "tr", "name": "Turkish" },
-			{ "abbr": "uk", "name": "Ukrainian" },
-			{ "abbr": "ur", "name": "Urdu" },
-			{ "abbr": "uz", "name": "Uzbek" },
-			{ "abbr": "uz-latn", "name": "Uzbek Latin" },
-			{ "abbr": "vi", "name": "Vietnamese" },
-			{ "abbr": "cy", "name": "Welsh" },
-			{ "abbr": "yo", "name": "Yoruba Nigeria" },
-			{ "abbr": "ss", "name": "siSwati" }
-		],
-		block = document.createElement("span");
+		{ "abbr": "af", "name": "Afrikaans" },
+		{ "abbr": "sq", "name": "Albanian" },
+		{ "abbr": "ar", "name": "Arabic" },
+		{ "abbr": "ar-dz", "name": "Arabic (Algeria)" },
+		{ "abbr": "ar-kw", "name": "Arabic (Kuwait)" },
+		{ "abbr": "ar-ly", "name": "Arabic (Libya)" },
+		{ "abbr": "ar-ma", "name": "Arabic (Morocco)" },
+		{ "abbr": "ar-sa", "name": "Arabic (Saudi Arabia)" },
+		{ "abbr": "ar-tn", "name": "Arabic (Tunisia)" },
+		{ "abbr": "hy-am", "name": "Armenian" },
+		{ "abbr": "az", "name": "Azerbaijani" },
+		{ "abbr": "bm", "name": "Bambara" },
+		{ "abbr": "eu", "name": "Basque" },
+		{ "abbr": "be", "name": "Belarusian" },
+		{ "abbr": "bn", "name": "Bengali" },
+		{ "abbr": "bn-bd", "name": "Bengali (Bangladesh)" },
+		{ "abbr": "bs", "name": "Bosnian" },
+		{ "abbr": "br", "name": "Breton" },
+		{ "abbr": "bg", "name": "Bulgarian" },
+		{ "abbr": "my", "name": "Burmese" },
+		{ "abbr": "km", "name": "Cambodian" },
+		{ "abbr": "ca", "name": "Catalan" },
+		{ "abbr": "tzm", "name": "Central Atlas Tamazight" },
+		{ "abbr": "tzm-latn", "name": "Central Atlas Tamazight Latin" },
+		{ "abbr": "zh-cn", "name": "Chinese (China)" },
+		{ "abbr": "zh-hk", "name": "Chinese (Hong Kong)" },
+		{ "abbr": "zh-mo", "name": "Chinese (Macau)" },
+		{ "abbr": "zh-tw", "name": "Chinese (Taiwan)" },
+		{ "abbr": "cv", "name": "Chuvash" },
+		{ "abbr": "hr", "name": "Croatian" },
+		{ "abbr": "cs", "name": "Czech" },
+		{ "abbr": "da", "name": "Danish" },
+		{ "abbr": "nl", "name": "Dutch" },
+		{ "abbr": "nl-be", "name": "Dutch (Belgium)" },
+		{ "abbr": "en-au", "name": "English (Australia)" },
+		{ "abbr": "en-ca", "name": "English (Canada)" },
+		{ "abbr": "en-in", "name": "English (India)" },
+		{ "abbr": "en-ie", "name": "English (Ireland)" },
+		{ "abbr": "en-il", "name": "English (Israel)" },
+		{ "abbr": "en-nz", "name": "English (New Zealand)" },
+		{ "abbr": "en-sg", "name": "English (Singapore)" },
+		{ "abbr": "en-gb", "name": "English (United Kingdom)" },
+		{ "abbr": "en", "name": "English (United States)" },
+		{ "abbr": "eo", "name": "Esperanto" },
+		{ "abbr": "et", "name": "Estonian" },
+		{ "abbr": "fo", "name": "Faroese" },
+		{ "abbr": "fil", "name": "Filipino" },
+		{ "abbr": "fi", "name": "Finnish" },
+		{ "abbr": "fr", "name": "French" },
+		{ "abbr": "fr-ca", "name": "French (Canada)" },
+		{ "abbr": "fr-ch", "name": "French (Switzerland)" },
+		{ "abbr": "fy", "name": "Frisian" },
+		{ "abbr": "gl", "name": "Galician" },
+		{ "abbr": "ka", "name": "Georgian" },
+		{ "abbr": "de", "name": "German" },
+		{ "abbr": "de-at", "name": "German (Austria)" },
+		{ "abbr": "de-ch", "name": "German (Switzerland)" },
+		{ "abbr": "el", "name": "Greek" },
+		{ "abbr": "gu", "name": "Gujarati" },
+		{ "abbr": "he", "name": "Hebrew" },
+		{ "abbr": "hi", "name": "Hindi" },
+		{ "abbr": "hu", "name": "Hungarian" },
+		{ "abbr": "is", "name": "Icelandic" },
+		{ "abbr": "id", "name": "Indonesian" },
+		{ "abbr": "ga", "name": "Irish or Irish Gaelic" },
+		{ "abbr": "it", "name": "Italian" },
+		{ "abbr": "it-ch", "name": "Italian (Switzerland)" },
+		{ "abbr": "ja", "name": "Japanese" },
+		{ "abbr": "jv", "name": "Javanese" },
+		{ "abbr": "kn", "name": "Kannada" },
+		{ "abbr": "kk", "name": "Kazakh" },
+		{ "abbr": "tlh", "name": "Klingon" },
+		{ "abbr": "gom-deva", "name": "Konkani Devanagari script" },
+		{ "abbr": "gom-latn", "name": "Konkani Latin script" },
+		{ "abbr": "ko", "name": "Korean" },
+		{ "abbr": "ku", "name": "Kurdish" },
+		{ "abbr": "ky", "name": "Kyrgyz" },
+		{ "abbr": "lo", "name": "Lao" },
+		{ "abbr": "lv", "name": "Latvian" },
+		{ "abbr": "lt", "name": "Lithuanian" },
+		{ "abbr": "lb", "name": "Luxembourgish" },
+		{ "abbr": "mk", "name": "Macedonian" },
+		{ "abbr": "ms-my", "name": "Malay" },
+		{ "abbr": "ms", "name": "Malay" },
+		{ "abbr": "ml", "name": "Malayalam" },
+		{ "abbr": "dv", "name": "Maldivian" },
+		{ "abbr": "mt", "name": "Maltese (Malta)" },
+		{ "abbr": "mi", "name": "Maori" },
+		{ "abbr": "mr", "name": "Marathi" },
+		{ "abbr": "mn", "name": "Mongolian" },
+		{ "abbr": "me", "name": "Montenegrin" },
+		{ "abbr": "ne", "name": "Nepalese" },
+		{ "abbr": "se", "name": "Northern Sami" },
+		{ "abbr": "nb", "name": "Norwegian Bokmål" },
+		{ "abbr": "nn", "name": "Nynorsk" },
+		{ "abbr": "oc-lnc", "name": "Occitan, lengadocian dialecte" },
+		{ "abbr": "fa", "name": "Persian" },
+		{ "abbr": "pl", "name": "Polish" },
+		{ "abbr": "pt", "name": "Portuguese" },
+		{ "abbr": "pt-br", "name": "Portuguese (Brazil)" },
+		{ "abbr": "x-pseudo", "name": "Pseudo" },
+		{ "abbr": "pa-in", "name": "Punjabi (India)" },
+		{ "abbr": "ro", "name": "Romanian" },
+		{ "abbr": "ru", "name": "Russian" },
+		{ "abbr": "gd", "name": "Scottish Gaelic" },
+		{ "abbr": "sr", "name": "Serbian" },
+		{ "abbr": "sr-cyrl", "name": "Serbian Cyrillic" },
+		{ "abbr": "sd", "name": "Sindhi" },
+		{ "abbr": "si", "name": "Sinhalese" },
+		{ "abbr": "sk", "name": "Slovak" },
+		{ "abbr": "sl", "name": "Slovenian" },
+		{ "abbr": "es", "name": "Spanish" },
+		{ "abbr": "es-do", "name": "Spanish (Dominican Republic)" },
+		{ "abbr": "es-mx", "name": "Spanish (Mexico)" },
+		{ "abbr": "es-us", "name": "Spanish (United States)" },
+		{ "abbr": "sw", "name": "Swahili" },
+		{ "abbr": "sv", "name": "Swedish" },
+		{ "abbr": "tl-ph", "name": "Tagalog (Philippines)" },
+		{ "abbr": "tg", "name": "Tajik" },
+		{ "abbr": "tzl", "name": "Talossan" },
+		{ "abbr": "ta", "name": "Tamil" },
+		{ "abbr": "te", "name": "Telugu" },
+		{ "abbr": "tet", "name": "Tetun Dili (East Timor)" },
+		{ "abbr": "th", "name": "Thai" },
+		{ "abbr": "bo", "name": "Tibetan" },
+		{ "abbr": "tr", "name": "Turkish" },
+		{ "abbr": "tk", "name": "Turkmen" },
+		{ "abbr": "uk", "name": "Ukrainian" },
+		{ "abbr": "ur", "name": "Urdu" },
+		{ "abbr": "ug-cn", "name": "Uyghur (China)" },
+		{ "abbr": "uz", "name": "Uzbek" },
+		{ "abbr": "uz-latn", "name": "Uzbek Latin" },
+		{ "abbr": "vi", "name": "Vietnamese" },
+		{ "abbr": "cy", "name": "Welsh" },
+		{ "abbr": "yo", "name": "Yoruba Nigeria" },
+		{ "abbr": "ss", "name": "siSwati" }
+	];
+	const block = document.createElement("span");
 	block.className = "ghst-time time";
 
 	function staticTime(tempFormat) {
@@ -161,8 +182,8 @@
 			: "relative-time, time-ago";
 		if ($(selector)) {
 			let indx = 0;
-			const els = $$(selector),
-				len = els.length;
+			const els = $$(selector);
+			const len = els.length;
 
 			// loop with delay to allow user interaction
 			const loop = () => {
@@ -177,15 +198,15 @@
 					time = el.getAttribute("datetime") || "";
 					if (el && time) {
 						if (tempFormat) {
-						    formatted = moment(time).format(tempFormat);
+							formatted = moment(time).format(tempFormat);
 							el.textContent = formatted;
-						    el.title = formatted;
+							el.title = formatted;
 						} else {
-						    formatted = moment(time).format(timeFormat);
+							formatted = moment(time).format(timeFormat);
 							node = block.cloneNode(true);
 							node.setAttribute("datetime", time);
 							node.textContent = formatted;
-						    node.title = formatted;
+							node.title = formatted;
 							// el.parentElement may be null sometimes when using browser
 							// back arrow
 							if (el.parentElement) {
@@ -198,9 +219,7 @@
 					indx++;
 				}
 				if (indx < len) {
-					setTimeout(() => {
-						loop();
-					}, 200);
+					requestAnimationFrame(loop);
 				}
 			};
 			loop();
@@ -263,8 +282,8 @@
 				</div>
 			</div>`;
 		$("body").appendChild(div);
-		on("#ghst-settings", "click", closePanel);
-		on("body", "keyup", event => {
+		on($("#ghst-settings"), "click", closePanel);
+		on($("body"), "keyup", event => {
 			if (
 				event.key === "Escape" &&
 				$("#ghst-settings").classList.contains("ghst-open")
@@ -276,17 +295,17 @@
 				update("save");
 			}
 		});
-		on("#ghst-settings-inner", "click", event => {
+		on($("#ghst-settings-inner"), "click", event => {
 			event.stopPropagation();
 			event.preventDefault();
 		});
-		on("#ghst-save", "click", () => {
+		on($("#ghst-save"), "click", () => {
 			closePanel();
 			update("save");
 		});
-		on("#ghst-locale", "change", update);
-		on("#ghst-format", "change", update);
-		on("#ghst-cancel", "click", closePanel);
+		on($("#ghst-locale"), "change", update);
+		on($("#ghst-format"), "change", update);
+		on($("#ghst-cancel"), "click", closePanel);
 	}
 
 	function closePanel(event) {
@@ -301,8 +320,8 @@
 			$("#ghst-locale").value = locale;
 			$("#ghst-format").value = timeFormat;
 		}
-		let loc = $("#ghst-locale").value || "en",
-			time = $("#ghst-format").value || "LLL";
+		let loc = $("#ghst-locale").value || "en";
+		let time = $("#ghst-format").value || "LLL";
 		if (mode === "save") {
 			timeFormat = time;
 			locale = loc;
@@ -312,18 +331,6 @@
 		moment.locale(loc);
 		staticTime(time);
 		return false;
-	}
-
-	function $(str, el) {
-		return (el || document).querySelector(str);
-	}
-
-	function $$(str, el) {
-		return Array.from((el || document).querySelectorAll(str));
-	}
-
-	function on(el, name, handler) {
-		$(el).addEventListener(name, handler);
 	}
 
 	function init() {
