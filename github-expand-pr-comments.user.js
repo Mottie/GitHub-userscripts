b8e10d3604e83d02c590f70b877a227f450ef6d5
konstantin <constantinvk@gmail.com>
2023-10-12 12:09:34 +0200

    Use tabs

--- github-expand-pr-comments.user.js
+++ github-expand-pr-comments.user.js
@@ -15,7 +15,7 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+	'use strict';
 
 	const ButtonPlacement = {
 		LeftFixed: "leftFixed",
@@ -37,84 +37,84 @@
 	const expandButtonExpandText = "Expand all comments";
 	const expandButtonExpandedText = "Expanded";
 	const expandingButtonExpandedText = "Expanding...";
-    const defaultNumberOffRetriesWithoutEffectBeforeStop = 5;
-    const intervalBetweenRetriesMs = 2000;
+	const defaultNumberOffRetriesWithoutEffectBeforeStop = 5;
+	const intervalBetweenRetriesMs = 2000;
 
-    function expandAllButtonsRecursive(notEffectiveRetriesLeftToDo, accumulatingNumberOfExpandedFragments)
-    {
-        const expandButton = document.getElementById(expandButtonId);
+	function expandAllButtonsRecursive(notEffectiveRetriesLeftToDo, accumulatingNumberOfExpandedFragments)
+	{
+		const expandButton = document.getElementById(expandButtonId);
 
 		if(DEBUG){
 			console.log(expandButton);
 		}
 
-        if(notEffectiveRetriesLeftToDo === 0){
+		if(notEffectiveRetriesLeftToDo === 0){
 			// Done
 			if(afterExpandMode === AfterExpandedBehavior.ExpandedText){
 				expandButton.innerHTML = expandButtonExpandedText;
 			}else if(afterExpandMode === AfterExpandedBehavior.RemoveButton){
 				expandButton.display = "none";
 			}
-        }
+		}
 		else if(notEffectiveRetriesLeftToDo > 0) {
 			// Continue expanding
-            expandButton.innerHTML = expandingButtonExpandedText + " (" + accumulatingNumberOfExpandedFragments + ")";
-            expandButton.disabled = true;
-        }
+			expandButton.innerHTML = expandingButtonExpandedText + " (" + accumulatingNumberOfExpandedFragments + ")";
+			expandButton.disabled = true;
+		}
 
 		// Find all visible fragments to expand
-        const buttons1 = document.evaluate("//button[contains(normalize-space(text()), 'hidden items')]", document, null, XPathResult.ANY_TYPE, null );
-        const buttons2 = document.evaluate("//button[contains(normalize-space(text()), 'hidden conversations')]", document, null, XPathResult.ANY_TYPE, null );
-        const allButtons = [];
+		const buttons1 = document.evaluate("//button[contains(normalize-space(text()), 'hidden items')]", document, null, XPathResult.ANY_TYPE, null );
+		const buttons2 = document.evaluate("//button[contains(normalize-space(text()), 'hidden conversations')]", document, null, XPathResult.ANY_TYPE, null );
+		const allButtons = [];
 
 		let cur;
-        while(cur = buttons1.iterateNext()){
-            allButtons.push(cur);
-        }
-        while(cur = buttons2.iterateNext()){
-            allButtons.push(cur);
-        }
+		while(cur = buttons1.iterateNext()){
+			allButtons.push(cur);
+		}
+		while(cur = buttons2.iterateNext()){
+			allButtons.push(cur);
+		}
 
-        let numberOfExpandedFragmentsOnIteration = 0;
-        let updatedNotEffectiveRetriesLeftToDo = notEffectiveRetriesLeftToDo;
+		let numberOfExpandedFragmentsOnIteration = 0;
+		let updatedNotEffectiveRetriesLeftToDo = notEffectiveRetriesLeftToDo;
 
 		allButtons.forEach((currentButton)=>{
-            if(DEBUG){
+			if(DEBUG){
 				console.log(currentButton);
 			}
-            currentButton.click(); // Emulate click to expand a fragment
-            numberOfExpandedFragmentsOnIteration ++;
-            return 1;
-        })
+			currentButton.click(); // Emulate click to expand a fragment
+			numberOfExpandedFragmentsOnIteration ++;
+			return 1;
+		})
 
-        let isNextIterationNeeded = true;
-        if(numberOfExpandedFragmentsOnIteration > 0){
-            updatedNotEffectiveRetriesLeftToDo = defaultNumberOffRetriesWithoutEffectBeforeStop;
-        }else if(numberOfExpandedFragmentsOnIteration == 0 && updatedNotEffectiveRetriesLeftToDo > 0){
-            updatedNotEffectiveRetriesLeftToDo --;
-        }else if(numberOfExpandedFragmentsOnIteration == 0 && updatedNotEffectiveRetriesLeftToDo == 0){
-            isNextIterationNeeded = false;
-        }
+		let isNextIterationNeeded = true;
+		if(numberOfExpandedFragmentsOnIteration > 0){
+			updatedNotEffectiveRetriesLeftToDo = defaultNumberOffRetriesWithoutEffectBeforeStop;
+		}else if(numberOfExpandedFragmentsOnIteration == 0 && updatedNotEffectiveRetriesLeftToDo > 0){
+			updatedNotEffectiveRetriesLeftToDo --;
+		}else if(numberOfExpandedFragmentsOnIteration == 0 && updatedNotEffectiveRetriesLeftToDo == 0){
+			isNextIterationNeeded = false;
+		}
 
-        if(isNextIterationNeeded){
-            setTimeout(expandAllButtonsRecursive, intervalBetweenRetriesMs, updatedNotEffectiveRetriesLeftToDo, accumulatingNumberOfExpandedFragments+numberOfExpandedFragmentsOnIteration);
-        }
-    }
+		if(isNextIterationNeeded){
+			setTimeout(expandAllButtonsRecursive, intervalBetweenRetriesMs, updatedNotEffectiveRetriesLeftToDo, accumulatingNumberOfExpandedFragments+numberOfExpandedFragmentsOnIteration);
+		}
+	}
 
-    function addExpandButton(buttonText, onclick) {
+	function addExpandButton(buttonText, onclick) {
 		let cssObj = {};
 
 		if(buttonPlaceMode === ButtonPlacement.LeftFixed){
 			cssObj = {position: 'fixed', top: '150px', left:'30px', 'z-index': 3};
 		}
 
-        let button = document.createElement('button'), btnStyle = button.style;
-        document.body.appendChild(button);
-        button.innerHTML = buttonText;
-        button.id = expandButtonId;
-        button.onclick = onclick;
-        Object.keys(cssObj).forEach(key => {btnStyle[key] = cssObj[key]; return 1;});
-        button.className = buttonGithubClassName;
+		let button = document.createElement('button'), btnStyle = button.style;
+		document.body.appendChild(button);
+		button.innerHTML = buttonText;
+		button.id = expandButtonId;
+		button.onclick = onclick;
+		Object.keys(cssObj).forEach(key => {btnStyle[key] = cssObj[key]; return 1;});
+		button.className = buttonGithubClassName;
 
 		if(buttonPlaceMode === ButtonPlacement.RightSidebar)
 		{
@@ -126,18 +126,18 @@
 		}
 
 		return button;
-    }
+	}
 
-    function expandButtonClickFunction() {
-        expandAllButtonsRecursive(defaultNumberOffRetriesWithoutEffectBeforeStop, 0);
-    }
+	function expandButtonClickFunction() {
+		expandAllButtonsRecursive(defaultNumberOffRetriesWithoutEffectBeforeStop, 0);
+	}
 
-    window.addEventListener('load', () => {
-        const btn = addExpandButton(expandButtonExpandText, expandButtonClickFunction);
+	window.addEventListener('load', () => {
+		const btn = addExpandButton(expandButtonExpandText, expandButtonClickFunction);
 
 		if(DEBUG){
 			console.log("Expand button has been added", btn);
 		}
-    })
+	})
 
 })();
